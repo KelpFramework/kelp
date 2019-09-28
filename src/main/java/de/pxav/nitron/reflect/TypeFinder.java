24422579a216ab76d317bcd5d264c6f24ee2edb5
pxav <pxav.studios@gmail.com>
2019-09-28 21:40:41 +0200

    Implement TypeFinder to search for classes matching certain criteria

--- src/main/java/de/pxav/nitron/reflect/TypeFinder.java
+++ src/main/java/de/pxav/nitron/reflect/TypeFinder.java
@@ -1,9 +1,58 @@
 package de.pxav.nitron.reflect;
 
+import com.google.common.base.Preconditions;
+import com.google.common.reflect.ClassPath;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import de.pxav.nitron.KelpPlugin;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * A class description goes here.
  *
  * @author pxav
  */
+@Singleton
 public class TypeFinder {
+
+  private ClassPath classPath;
+
+  @Inject
+  public TypeFinder() {
+    try {
+      this.classPath = ClassPath.from(KelpPlugin.class.getClassLoader());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public Stream<? extends Class<?>> filter(String[] packageNames, TypeCriterion... typeCriteria) {
+    Preconditions.checkNotNull(packageNames);
+    Preconditions.checkNotNull(typeCriteria);
+    return (packageNames.length == 0
+            ? this.classPath.getAllClasses()
+            : Arrays.stream(packageNames)
+            .flatMap(
+                    packageName -> this.classPath.getTopLevelClassesRecursive(packageName).stream())
+            .collect(Collectors.toList()))
+            .stream()
+            .map(
+                    classInfo -> {
+                      try {
+                        return classInfo.load();
+                      } catch (Throwable ignore) {}
+                      return null;
+                    })
+            .filter(Objects::nonNull)
+            .filter(
+                    clazz ->
+                            Arrays.stream(typeCriteria).allMatch(typeFilter -> typeFilter.matches(clazz)));
+  }
+
 }
