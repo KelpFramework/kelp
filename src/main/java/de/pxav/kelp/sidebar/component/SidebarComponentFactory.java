4d3479b7257f1aedf7f22bcbdc7e15db0238b3d6
pxav <pxav.studios@gmail.com>
2019-10-25 17:47:52 +0200

    Sidebar components can now be created easier via factory

--- src/main/java/de/pxav/kelp/sidebar/component/SidebarComponentFactory.java
+++ src/main/java/de/pxav/kelp/sidebar/component/SidebarComponentFactory.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import de.pxav.kelp.logger.KelpLogger;
 import de.pxav.kelp.sidebar.SidebarUtils;
+import org.bukkit.ChatColor;
 
 /**
  * This class is used to easily create sidebar components.
@@ -26,20 +27,84 @@ public final class SidebarComponentFactory {
     this.sidebarUtils = sidebarUtils;
   }
 
-  public SimpleTextComponent newSimpleTextComponent() {
+  public SimpleTextComponent simpleTextComponent() {
     return new SimpleTextComponent(sidebarUtils, kelpLogger);
   }
 
-  public EmptyLineComponent newEmptyLineComponent() {
+  public SimpleTextComponent simpleTextComponent(String text) {
+    return new SimpleTextComponent(sidebarUtils, kelpLogger)
+            .text(text);
+  }
+
+  public SimpleTextComponent simpleTextComponent(String text, int line) {
+    return new SimpleTextComponent(sidebarUtils, kelpLogger)
+            .text(text)
+            .line(line);
+  }
+
+  public EmptyLineComponent emptyLineComponent() {
     return new EmptyLineComponent(sidebarUtils);
   }
 
-  public EmptyLineComponent newEmptyLineComponent(int line) {
+  public EmptyLineComponent emptyLineComponent(int line) {
     return new EmptyLineComponent(sidebarUtils).score(line);
   }
 
-  public LineSeparatorComponent newLineSeparatorComponent() {
+  public LineSeparatorComponent lineSeparatorComponent() {
     return new LineSeparatorComponent(sidebarUtils);
+  }
+
+  public LineSeparatorComponent lineSeparatorComponent(char symbol) {
+    return new LineSeparatorComponent(sidebarUtils).symbol(symbol);
+  }
+
+  public LineSeparatorComponent lineSeparatorComponent(int line, char symbol) {
+    return new LineSeparatorComponent(sidebarUtils)
+            .symbol(symbol)
+            .score(line);
+  }
+
+  public LineSeparatorComponent lineSeparatorComponent(char symbol, ChatColor... colors) {
+    return new LineSeparatorComponent(sidebarUtils).symbol(symbol).color(colors);
+  }
+
+  public LineSeparatorComponent lineSeparatorComponent(int line, char symbol, ChatColor... colors) {
+    return new LineSeparatorComponent(sidebarUtils)
+            .symbol(symbol)
+            .color(colors)
+            .score(line);
+  }
+
+  public LineSeparatorComponent lineSeparatorComponent(ChatColor... colors) {
+    return new LineSeparatorComponent(sidebarUtils)
+            .color(colors);
+  }
+
+  public LineSeparatorComponent lineSeparatorComponent(int line, ChatColor... colors) {
+    return new LineSeparatorComponent(sidebarUtils)
+            .color(colors)
+            .score(line);
+  }
+
+  public LineSeparatorComponent lineSeparatorComponent(char symbol, int length, ChatColor... colors) {
+    return new LineSeparatorComponent(sidebarUtils)
+            .color(colors)
+            .symbol(symbol)
+            .length(length);
+  }
+
+  public LineSeparatorComponent lineSeparatorComponent(int line, char symbol, int length, ChatColor... colors) {
+    return new LineSeparatorComponent(sidebarUtils)
+            .color(colors)
+            .symbol(symbol)
+            .length(length)
+            .score(line);
+  }
+
+  public LineSeparatorComponent lineSeparatorComponent(int line, int length) {
+    return new LineSeparatorComponent(sidebarUtils)
+            .score(line)
+            .length(length);
   }
 
 }
