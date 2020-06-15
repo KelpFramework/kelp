9a38ff9a5f86442ccbed09d4daea1aa1fb41869b
pxav <pxav.studios@gmail.com>
2020-06-15 10:31:00 +0200

    Add documentation for 1.8 player version implementation.

--- v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/player/VersionedPlayer.java
+++ v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/player/VersionedPlayer.java
@@ -19,6 +19,7 @@ import java.util.UUID;
  * A class description goes here.
  *
  * @author pxav
+ * @author Etrayed
  */
 @Versioned
 public class VersionedPlayer extends PlayerVersionTemplate {
@@ -30,6 +31,17 @@ public class VersionedPlayer extends PlayerVersionTemplate {
     this.soundRepository = soundRepository;
   }
 
+  /**
+   * Sends a title to a player. A title is a big text displayed
+   * right in the middle of the player's screen.
+   *
+   * @param player The player who should see the title.
+   * @param title The upper title text (will be displayed slightly bigger than the sub title).
+   * @param subTitle The lower title text (will be displayed slightly smaller than the main title).
+   * @param fadeIn How long should it take to fade the title in? (in ticks)
+   * @param stay How long should the title stay in 100% opacity? (in ticks)
+   * @param fadeOut How long should it take to fade the title out? (in ticks)
+   */
   @Override
   public void sendTitle(Player player, String title, String subTitle, int fadeIn, int stay, int fadeOut) {
     final IChatBaseComponent titleComponent = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + title + "\"}");
@@ -45,6 +57,14 @@ public class VersionedPlayer extends PlayerVersionTemplate {
     this.sendPacket(subtitlePacket, player);
   }
 
+  /**
+   * Sends an action bar message to the player.
+   * The action bar is a line of text, which is displayed
+   * above the player's hotbar.
+   *
+   * @param player  The player who should receive the message.
+   * @param message The message you want to send.
+   */
   @Override
   public void sendActionBar(Player player, String message) {
     PacketPlayOutChat packet =
@@ -53,6 +73,18 @@ public class VersionedPlayer extends PlayerVersionTemplate {
     sendPacket(packet, player);
   }
 
+  /**
+   * Sets the tab header and footer of player. The tab header is
+   * a text displayed above the player list in the tab, while
+   * the tab footer is a message displayed below the player list.
+   *
+   * The messages may contain '\n' to create new lines inside the
+   * message.
+   *
+   * @param player The player who should see the messages.
+   * @param header The header message you want to send.
+   * @param footer The footer message you want to send.
+   */
   @Override
   public void sendTabHeaderAndFooter(Player player, String header, String footer) {
     if (header == null) header = "";
@@ -75,31 +107,68 @@ public class VersionedPlayer extends PlayerVersionTemplate {
     sendPacket(packet, player);
   }
 
+  /**
+   * Plays a sound to the player.
+   *
+   * @param player    The player who should hear the sound.
+   * @param sound     The sound you want to play.
+   * @param location  The location, where the sound should come from.
+   * @param volume    How loud the sound should be.
+   * @param pitch     How strong the sound should be pitched.
+   */
   @Override
   public void playSound(Player player, KelpSound sound, Location location, float volume, float pitch) {
     Sound bukkitSound = Sound.valueOf(soundRepository.getSound(sound));
     player.playSound(player.getLocation(), bukkitSound, volume, pitch);
   }
 
+  /**
+   * Sets the player's health.
+   *
+   * @param player The player whose health you want to change.
+   * @param health How many health points the player should have.
+   *               2 health points equal 1 heart.
+   *               So 20 health points equal the full 10 hearts.
+   */
   @Override
   public void setHealth(Player player, int health) {
     player.setHealth(health);
   }
 
+  /**
+   * Teleports the player to the given location.
+   *
+   * @param player    The player you want to teleport.
+   * @param location  The location you want to teleport the player to.
+   */
   @Override
   public void teleport(Player player, Location location) {
     player.teleport(location);
   }
 
+  /**
+   * @param player The player whose UUID you want to get.
+   * @return The player's uuid.
+   */
   public UUID getUniqueId(Player player) {
     return player.getUniqueId();
   }
 
+  /**
+   * @param player The player whose location you want to get
+   * @return The location the player is currently at.
+   */
   @Override
   public Location getLocation(Player player) {
     return null;
   }
 
+  /**
+   * Checks if the player is currently located in water.
+   *
+   * @param player The player you want to check.
+   * @return {@code true} if the player is currently in water.
+   */
   @Override
   public boolean isInWater(Player player) {
     try {
@@ -115,6 +184,12 @@ public class VersionedPlayer extends PlayerVersionTemplate {
     return false;
   }
 
+  /**
+   * Checks if the player is currently stuck in a cobweb.
+   *
+   * @param player The player you want to check.
+   * @return {@code true} if the player is currently stuck in a cobweb.
+   */
   @Override
   public boolean isInCobweb(Player player) {
     try {
@@ -130,6 +205,12 @@ public class VersionedPlayer extends PlayerVersionTemplate {
     return false;
   }
 
+  /**
+   * Sends the given packet to the given player.
+   *
+   * @param packet The packet you want to send.
+   * @param player The player who should receive the packet.
+   */
   private void sendPacket(Packet packet, Player player) {
     ((CraftPlayer)player).getHandle().playerConnection.sendPacket(packet);
   }
