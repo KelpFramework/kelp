af40f8d6981bd63a02a75909cb42513e10c2d62c
pxav <pxav.studios@gmail.com>
2020-06-17 22:46:20 +0200

    Add settings packet interceptor to 1.8 module

---
+++ v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/packet/GlobalPacketListener.java
@@ -0,0 +1,89 @@
+package de.pxav.kelp.implementation1_8.packet;
+
+import com.google.inject.Inject;
+import de.pxav.kelp.core.player.KelpPlayer;
+import de.pxav.kelp.core.player.KelpPlayerRepository;
+import de.pxav.kelp.core.player.PlayerChatVisibility;
+import de.pxav.kelp.core.reflect.ReflectionUtil;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.ChannelPromise;
+import net.minecraft.server.v1_8_R3.PacketPlayInSettings;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+/**
+ * A class description goes here.
+ *
+ * @author pxav
+ */
+public class GlobalPacketListener {
+
+  private KelpPlayerRepository playerRepository;
+  private ReflectionUtil reflectionUtil;
+
+  @Inject
+  public GlobalPacketListener(KelpPlayerRepository playerRepository, ReflectionUtil reflectionUtil) {
+    this.playerRepository = playerRepository;
+    this.reflectionUtil = reflectionUtil;
+  }
+
+  @EventHandler
+  public void handlePlayerJoin(PlayerJoinEvent event) {
+    injectPacketListener(event.getPlayer());
+  }
+
+  /**
+   * Creates a new packet listener.
+   *
+   * @param player
+   */
+  private void injectPacketListener(Player player) {
+    ChannelDuplexHandler channelDuplexHandler = new ChannelDuplexHandler() {
+
+      @Override
+      public void channelRead(ChannelHandlerContext channelHandlerContext, Object packet) throws Exception {
+        if (packet instanceof PacketPlayInSettings) {
+
+          PacketPlayInSettings settingsPacket = (PacketPlayInSettings) packet;
+          String language = String.valueOf(reflectionUtil.getValue(settingsPacket, "a"));
+          int viewDistance = Integer.parseInt(String.valueOf(reflectionUtil.getValue(settingsPacket, "b")));
+          boolean chatColorEnabled = Boolean.parseBoolean(String.valueOf(reflectionUtil.getValue(settingsPacket, "d")));
+          PlayerChatVisibility chatVisibility = PlayerChatVisibility.SHOW_ALL_MESSAGES;
+          switch (String.valueOf(reflectionUtil.getValue(settingsPacket, "c"))) {
+            case "FULL":
+              chatVisibility = PlayerChatVisibility.SHOW_ALL_MESSAGES;
+            case "SYSTEM":
+              chatVisibility = PlayerChatVisibility.COMMANDS_ONLY;
+            case "HIDDEN":
+              chatVisibility = PlayerChatVisibility.HIDDEN;
+          }
+
+          KelpPlayer kelpPlayer = playerRepository.getKelpPlayer(player);
+          kelpPlayer
+            .setClientLanguageInternally(language)
+            .setClientViewDistanceInternally(viewDistance)
+            .setPlayerChatColorEnabledInternally(chatColorEnabled)
+            .setPlayerChatVisibilityInternally(chatVisibility);
+          playerRepository.addOrUpdatePlayer(kelpPlayer.getUUID(), kelpPlayer);
+
+          return;
+        }
+        super.channelRead(channelHandlerContext, packet);
+      }
+
+      @Override
+      public void write(ChannelHandlerContext channelHandlerContext, Object packet, ChannelPromise channelPromise) throws Exception {
+        super.write(channelHandlerContext, packet, channelPromise);
+      }
+
+    };
+
+    ChannelPipeline pipeline = ((CraftPlayer)player).getHandle().playerConnection.networkManager.channel.pipeline();
+    pipeline.addBefore("packet_handler", player.getName(), channelDuplexHandler);
+  }
+
+}
---
+++ v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/player/PlayerCreationListener.java
@@ -0,0 +1,37 @@
+package de.pxav.kelp.implementation1_8.player;
+
+import com.google.inject.Inject;
+import de.pxav.kelp.core.player.KelpPlayer;
+import de.pxav.kelp.core.player.KelpPlayerRepository;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+/**
+ * A class description goes here.
+ *
+ * @author pxav
+ */
+public class PlayerCreationListener {
+
+  private KelpPlayerRepository kelpPlayerRepository;
+
+  @Inject
+  public PlayerCreationListener(KelpPlayerRepository kelpPlayerRepository) {
+    this.kelpPlayerRepository = kelpPlayerRepository;
+  }
+
+  @EventHandler
+  public void handlePlayerLogin(PlayerLoginEvent event) {
+    KelpPlayer kelpPlayer = kelpPlayerRepository.newKelpPlayer(event.getPlayer());
+    kelpPlayerRepository.addOrUpdatePlayer(kelpPlayer.getUUID(), kelpPlayer);
+  }
+
+  public void createOnStartup() {
+    Bukkit.getOnlinePlayers().forEach(current -> {
+      KelpPlayer kelpPlayer = kelpPlayerRepository.newKelpPlayer(current);
+      kelpPlayerRepository.addOrUpdatePlayer(kelpPlayer.getUUID(), kelpPlayer);
+    });
+  }
+
+}
