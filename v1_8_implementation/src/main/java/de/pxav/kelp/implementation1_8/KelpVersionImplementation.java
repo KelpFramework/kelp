8431664a5d74d0b1b542c0b606c27e6171b1563f
pxav <pxav.studios@gmail.com>
2020-06-17 14:01:18 +0200

    Fix 1.8 implementation main class so that it can be detected correctly by the core module

--- v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/KelpVersionImplementation.java
+++ v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/KelpVersionImplementation.java
@@ -1,7 +1,9 @@
 package de.pxav.kelp.implementation1_8;
 
+import com.google.inject.Singleton;
 import de.pxav.kelp.core.application.KelpApplication;
 import de.pxav.kelp.core.application.NewKelpApplication;
+import de.pxav.kelp.core.logger.KelpLogger;
 import de.pxav.kelp.core.sidebar.CreateSidebar;
 import de.pxav.kelp.core.sidebar.type.SimpleSidebar;
 import de.pxav.kelp.core.version.KelpVersion;
@@ -12,11 +14,24 @@ import de.pxav.kelp.core.version.VersionImplementation;
  *
  * @author pxav
  */
+@Singleton
+@VersionImplementation(value = {
+  KelpVersion.MC_1_8_0,
+  KelpVersion.MC_1_8_3,
+  KelpVersion.MC_1_8_4,
+  KelpVersion.MC_1_8_5,
+  KelpVersion.MC_1_8_6,
+  KelpVersion.MC_1_8_7,
+  KelpVersion.MC_1_8_8
+}, authors = {
+  "pxav",
+  "Etrayed"
+})
 public class KelpVersionImplementation extends KelpApplication {
 
   @Override
   public void onEnable() {
-
+    getInstance(KelpLogger.class).log("Enabling v1.8 version module...");
   }
 
   @Override
