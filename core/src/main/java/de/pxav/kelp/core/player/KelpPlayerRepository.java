1704575d29487a93cdf87415de06310956d8c714
pxav <pxav.studios@gmail.com>
2020-06-17 22:44:22 +0200

    Replace PlayerFactory with a new player repository

--- core/src/main/java/de/pxav/kelp/core/player/KelpPlayerRepository.java
+++ core/src/main/java/de/pxav/kelp/core/player/KelpPlayerRepository.java
@@ -1,14 +1,19 @@
 package de.pxav.kelp.core.player;
 
+import com.google.common.collect.Maps;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import de.pxav.kelp.core.inventory.KelpInventoryRepository;
 import de.pxav.kelp.core.logger.KelpLogger;
 import de.pxav.kelp.core.logger.LogLevel;
 import de.pxav.kelp.core.sidebar.SidebarRepository;
+import net.minecraft.server.v1_14_R1.PacketHandshakingInListener;
+import net.minecraft.server.v1_14_R1.PacketHandshakingInSetProtocol;
 import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_14_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -17,7 +22,9 @@ import java.util.UUID;
  * @author pxav
  */
 @Singleton
-public class PlayerFactory {
+public class KelpPlayerRepository {
+
+  private Map<UUID, KelpPlayer> kelpPlayers = Maps.newHashMap();
 
   private PlayerVersionTemplate playerVersionTemplate;
   private SidebarRepository sidebarRepository;
@@ -25,7 +32,7 @@ public class PlayerFactory {
   private KelpLogger logger;
 
   @Inject
-  public PlayerFactory(PlayerVersionTemplate playerVersionTemplate,
+  public KelpPlayerRepository(PlayerVersionTemplate playerVersionTemplate,
                        KelpLogger logger,
                        SidebarRepository sidebarRepository,
                        KelpInventoryRepository inventoryRepository) {
@@ -35,24 +42,37 @@ public class PlayerFactory {
     this.inventoryRepository = inventoryRepository;
   }
 
+  public void addOrUpdatePlayer(UUID uuid, KelpPlayer updatedPlayer) {
+    this.kelpPlayers.put(uuid, updatedPlayer);
+  }
+
+  public KelpPlayer getKelpPlayer(UUID uuid) {
+    return this.kelpPlayers.getOrDefault(uuid, null);
+  }
+
+  public KelpPlayer getKelpPlayer(Player bukkitPlayer) {
+    UUID uuid = playerVersionTemplate.getUniqueId(bukkitPlayer);
+    return getKelpPlayer(uuid);
+  }
+
   public KelpPlayer newKelpPlayer(Player bukkitPlayer) {
     return new KelpPlayer(bukkitPlayer,
-            playerVersionTemplate,
-            sidebarRepository,
-            inventoryRepository);
+      playerVersionTemplate,
+      sidebarRepository,
+      inventoryRepository);
   }
 
   public KelpPlayer newKelpPlayer(UUID uuid) {
     Player bukkitPlayer = Bukkit.getPlayer(uuid);
     if (bukkitPlayer == null) {
       logger.log(LogLevel.WARNING, "Given player UUID for getting a new KelpPlayer failed." +
-              "This player is not online, returning null!");
+        "This player is not online, returning null!");
       return null;
     }
     return new KelpPlayer(bukkitPlayer,
-            playerVersionTemplate,
-            sidebarRepository,
-            inventoryRepository);
+      playerVersionTemplate,
+      sidebarRepository,
+      inventoryRepository);
   }
 
 }
