2f5c1bc5e0df93535fe1099cafe4d141b8455fba
pxav <pxav.studios@gmail.com>
2020-06-26 16:35:00 +0200

    Add sample classes for guardian entities (special entity types)

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/ElderGuardianEntity.java
@@ -0,0 +1,11 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.KelpEntity;
+
+/**
+ * A class description goes here.
+ *
+ * @author pxav
+ */
+public class ElderGuardianEntity extends KelpEntity {
+}
