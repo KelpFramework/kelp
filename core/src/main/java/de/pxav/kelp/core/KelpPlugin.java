db2cad9ba39b8f61afeed11b540a862ca38fc495
pxav <pxav.studios@gmail.com>
2020-08-03 10:34:44 +0200

    Update version tags in POMs and plugin information

--- core/src/main/java/de/pxav/kelp/core/KelpPlugin.java
+++ core/src/main/java/de/pxav/kelp/core/KelpPlugin.java
@@ -31,7 +31,7 @@ import java.util.logging.Level;
  *
  * @author pxav
  */
-@Plugin(name = "Kelp", version = "0.0.1-SNAPSHOT")
+@Plugin(name = "Kelp", version = "0.0.2-SNAPSHOT")
 @Author("pxav")
 @Description("A cross version spigot framework.")
 @Singleton
