07218759794ee7f43ea4bceff4560505c4e77c03
pxav <pxav.studios@gmail.com>
2020-08-09 14:59:22 +0200

    Particle effects are now canceled when all viewers quit

--- core/src/main/java/de/pxav/kelp/core/particle/effect/ParticleEffectRepository.java
+++ core/src/main/java/de/pxav/kelp/core/particle/effect/ParticleEffectRepository.java
@@ -1,13 +1,16 @@
 package de.pxav.kelp.core.particle.effect;
 
-import com.google.common.collect.Maps;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import de.pxav.kelp.core.player.KelpPlayer;
+import de.pxav.kelp.core.player.KelpPlayerRepository;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 /**
@@ -19,12 +22,18 @@ import java.util.concurrent.ScheduledExecutorService;
 public class ParticleEffectRepository {
 
   private ConcurrentHashMap<Collection<KelpPlayer>, ScheduledExecutorService> effectTimers;
+  private KelpPlayerRepository playerRepository;
+  private ExecutorService executorService;
 
-  public ParticleEffectRepository() {
+  @Inject
+  public ParticleEffectRepository(KelpPlayerRepository playerRepository) {
     this.effectTimers = new ConcurrentHashMap<>();
+    this.playerRepository = playerRepository;
+    this.executorService = Executors.newCachedThreadPool();
   }
 
   public void addTimer(Collection<KelpPlayer> players, ScheduledExecutorService executorService) {
+    this.effectTimers.put(players, executorService);
   }
 
   public void stopAllTimers() {
@@ -33,7 +42,24 @@ public class ParticleEffectRepository {
 
   @EventHandler
   public void handlePlayerQuit(PlayerQuitEvent event) {
+    executorService.execute(() -> {
+      KelpPlayer player = playerRepository.getKelpPlayer(event.getPlayer());
 
+      new ConcurrentHashMap<>(effectTimers).forEach((players, timer) -> {
+
+        if (players.isEmpty() || (players.size() == 1 && players.contains(player))) {
+          timer.shutdownNow();
+          effectTimers.remove(players);
+          return;
+        }
+
+        if (players.contains(player)) {
+          players.remove(player);
+           effectTimers.put(players, timer);
+        }
+
+      });
+    });
   }
 
 }
