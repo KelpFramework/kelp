e31811bae20f4dcef933ad6178fdb899c49e6e94
pxav <pxav.studios@gmail.com>
2020-10-11 22:47:50 +0200

    Add documentation for listener util classes

--- core/src/main/java/de/pxav/kelp/core/listener/ConditionalExpiryTestStage.java
+++ core/src/main/java/de/pxav/kelp/core/listener/ConditionalExpiryTestStage.java
@@ -1,14 +1,35 @@
 package de.pxav.kelp.core.listener;
 
 /**
- * A class description goes here.
+ * This enum says in which execution stage a conditional expiry
+ * of an event should be checked.
+ *
+ * That means that if you set any conditional expiry ({@code #expireIf(condition)})
+ * the conditions are checked two times: before and after the code in the event
+ * handler
  *
  * @author pxav
  */
 public enum ConditionalExpiryTestStage {
 
+  /**
+   * The given condition is checked before the event handler is executed.
+   * If the condition is not fulfilled, the listener will be removed and
+   * the handler is not executed.
+   */
   BEFORE_HANDLER,
+
+  /**
+   * The given condition is checked after the event handler has been
+   * executed. Even if the condition is not fulfilled, the event handler
+   * will be executed at least once. After the handler has been executed,
+   * the listener is unregistered.
+   */
   AFTER_HANDLER,
+
+  /**
+   * The condition is checked both before and after the execution.
+   */
   ALWAYS
 
 }
--- core/src/main/java/de/pxav/kelp/core/listener/Subscribes.java
+++ core/src/main/java/de/pxav/kelp/core/listener/Subscribes.java
@@ -8,7 +8,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * A class description goes here.
+ * This annotation is used to mark methods which should be
+ * executed on a specific event. You can either listen to only
+ * one or multiple events.
+ *
+ * A useful example would be to subscribe to the join and quit event
+ * and update the new player count in the scoreboard for all players
+ * then.
  *
  * @author pxav
  */
@@ -16,7 +22,21 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Subscribes {
 
+  /**
+   * The classes of the events you want to listen for.
+   *
+   * @return The event classes.
+   */
   Class<? extends Event>[] value();
+
+  /**
+   * Enable whether Kelp should try to automatically inject
+   * common parameters such as a player for {@code PlayerEvents}.
+   * WARNING: This is still in experimental stage and is not safe.
+   * Please do not yet rely on the parameters passed by Kelp.
+   *
+   * @return whether parameters should be injected automatically.
+   */
   boolean injectParameters() default true;
 
 }
