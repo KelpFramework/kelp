616a906522a87f8d7e01a47aeb474ac4ea086475
pxav <pxav.studios@gmail.com>
2020-10-22 11:08:11 +0200

    Fix #31: Exception when player quits during server reload

--- v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/player/PlayerCreationListener.java
+++ v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/player/PlayerCreationListener.java
@@ -9,6 +9,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.NoSuchElementException;
 
 /**
  * A class description goes here.
@@ -34,15 +37,30 @@ public class PlayerCreationListener {
     kelpPlayerRepository.addOrUpdatePlayer(kelpPlayer.getUUID(), kelpPlayer);
   }
 
+  @EventHandler
+  public void handlePlayerQuit(PlayerQuitEvent event) {
+    kelpPlayerRepository.removeKelpPlayer(event.getPlayer().getUniqueId());
+  }
+
   public void createOnStartup() {
     Bukkit.getOnlinePlayers().forEach(current -> {
-      KelpPlayer kelpPlayer = kelpPlayerRepository.newKelpPlayer(current);
-      kelpPlayer.setClientViewDistanceInternally(Bukkit.getViewDistance());
-      kelpPlayer.setClientLanguageInternally("en_US");
-      kelpPlayer.setPlayerChatVisibilityInternally(PlayerChatVisibility.SHOW_ALL_MESSAGES);
-      kelpPlayer.setPlayerChatColorEnabledInternally(true);
-      kelpPlayerRepository.addOrUpdatePlayer(kelpPlayer.getUUID(), kelpPlayer);
-      globalPacketListener.injectPacketListener(current);
+      // catches NoSuchElementException which occurs when a player quits
+      // the server during a reload, because the server does not recognize
+      // the quit event at first. So the exception is caught and the player
+      // is removed from the cache again.
+      try {
+        KelpPlayer kelpPlayer = kelpPlayerRepository.newKelpPlayer(current);
+        kelpPlayer.setClientViewDistanceInternally(Bukkit.getViewDistance());
+        kelpPlayer.setClientLanguageInternally("en_US");
+        kelpPlayer.setPlayerChatVisibilityInternally(PlayerChatVisibility.SHOW_ALL_MESSAGES);
+        kelpPlayer.setPlayerChatColorEnabledInternally(true);
+        globalPacketListener.injectPacketListener(current);
+        kelpPlayerRepository.addOrUpdatePlayer(kelpPlayer.getUUID(), kelpPlayer);
+      } catch (NoSuchElementException ignore) {
+        // if the player has quit during the reload, immediately remove it
+        // from the cache again.
+        kelpPlayerRepository.removeKelpPlayer(current.getUniqueId());
+      }
     });
   }
 
