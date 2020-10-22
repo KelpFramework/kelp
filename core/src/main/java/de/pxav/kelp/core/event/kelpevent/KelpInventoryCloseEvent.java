520b1d0cfbc50c165a297c7ff913cd876f14926a
pxav <pxav.studios@gmail.com>
2020-10-22 10:34:32 +0200

    Add custom event which is called every time a kelp inventory is opened

---
+++ core/src/main/java/de/pxav/kelp/core/event/kelpevent/KelpInventoryCloseEvent.java
@@ -0,0 +1,37 @@
+package de.pxav.kelp.core.event.kelpevent;
+
+import de.pxav.kelp.core.inventory.type.KelpInventory;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+
+/**
+ * A class description goes here.
+ *
+ * @author pxav
+ */
+public class KelpInventoryCloseEvent extends PlayerEvent {
+
+  private static final HandlerList handlers = new HandlerList();
+  private KelpInventory inventory;
+  private boolean animated;
+
+  public KelpInventoryCloseEvent(Player who, KelpInventory inventory, boolean isAnimated) {
+    super(who);
+    this.inventory = inventory;
+    this.animated = isAnimated;
+  }
+
+  public KelpInventory getInventory() {
+    return this.inventory;
+  }
+
+  public boolean isAnimated() {
+    return animated;
+  }
+
+  @Override
+  public HandlerList getHandlers() {
+    return handlers;
+  }
+}
