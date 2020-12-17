9d5125de82b720c6126a6a05f59d20c5a91a9c24
pxav <pxav.studios@gmail.com>
2020-12-17 19:28:17 +0100

    Remove boss bar scheduler call from 1.8 implementation main class

--- v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/KelpVersionImplementation.java
+++ v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/KelpVersionImplementation.java
@@ -35,7 +35,6 @@ public class KelpVersionImplementation extends KelpApplication {
     getInstance(KelpLogger.class).log("Enabling v1.8 version module...");
     getInstance(EventHandlerRegistration.class).initialize("de.pxav.kelp.implementation1_8");
     getInstance(PlayerCreationListener.class).createOnStartup();
-    //getInstance(BossBarLocationUpdater.class).startScheduler();
   }
 
   @Override
