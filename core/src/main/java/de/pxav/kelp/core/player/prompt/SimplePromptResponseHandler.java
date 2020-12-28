3fd79ee76a7712fb6a264b88c70cf4ee636b968f
pxav <pxav.studios@gmail.com>
2020-12-28 19:16:20 +0100

    Add documentation for SimplePromptResponse handler

--- core/src/main/java/de/pxav/kelp/core/player/prompt/SimplePromptResponseHandler.java
+++ core/src/main/java/de/pxav/kelp/core/player/prompt/SimplePromptResponseHandler.java
@@ -1,12 +1,24 @@
 package de.pxav.kelp.core.player.prompt;
 
 /**
- * A class description goes here.
+ * This interface should be implemented by every class handling the input
+ * of a simple prompt (Chat or anvil). If you want to handle a sign prompt,
+ * please use {@link de.pxav.kelp.core.player.prompt.sign.SignPromptResponseHandler}.
  *
  * @author pxav
  */
 public interface SimplePromptResponseHandler {
 
+  /**
+   * This method is executed every time a player submits their prompt
+   * input to the server. Then you can check this input and decide whether
+   * it is valid and the prompt can be closed or whether the player
+   * has to try it again.
+   *
+   * @param input The input sent by the player.
+   * @return Whether the player's input was valid and the prompt may close
+   *         or whether the player has to try again.
+   */
   PromptResponseType accept(String input);
 
 }
