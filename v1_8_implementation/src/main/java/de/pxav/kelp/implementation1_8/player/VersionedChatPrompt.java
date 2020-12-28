5b23d11a1227c4d65499a6cad6a79690590a7924
pxav <pxav.studios@gmail.com>
2020-12-28 19:11:59 +0100

    Add documentation for 1.8 chat prompt implementation

--- v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/player/VersionedChatPrompt.java
+++ v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/player/VersionedChatPrompt.java
@@ -22,7 +22,9 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentMap;
 
 /**
- * A class description goes here.
+ * This is the {@code 1.8} version implementation of the chat prompt.
+ * This class contains the method to open the prompt to the player
+ * but also listens to the input and calls the corresponding input handler.
  *
  * @author pxav
  */
@@ -47,6 +49,7 @@ public class VersionedChatPrompt extends ChatPromptVersionTemplate {
     Player player = simpleChatPrompt.getPlayer();
     PromptTimeout timeout = simpleChatPrompt.getTimeout();
 
+    // configure timeout scheduler
     if (timeout != null) {
       UUID task = schedulerFactory.newDelayedScheduler()
         .withDelayOf(timeout.getTimeout())
@@ -55,6 +58,8 @@ public class VersionedChatPrompt extends ChatPromptVersionTemplate {
         .run((taskId -> {
           this.removeFromCache(player.getUniqueId());
 
+          // if the timeout code should be handled sync, jump back to the main thread
+          // as soon as the timeout happens. Until then, stay on async thread.
           if (timeout.getOnTimeout() != null && !timeout.isAsync()) {
             ServerMainThread.RunParallel.run(() -> {
               timeout.getOnTimeout().run();
@@ -70,9 +75,19 @@ public class VersionedChatPrompt extends ChatPromptVersionTemplate {
       this.playerTimeouts.put(player.getUniqueId(), timeout);
     }
 
+    // add it to the cache so that the event handler is able to notice
+    // the player is in a prompt.
     this.chatPrompts.put(player.getUniqueId(), simpleChatPrompt);
   }
 
+  /**
+   * This method is triggered when a player sends a chat message.
+   * It calls the message handler of the prompt and prompts the player
+   * again if needed. It also checks if the player wants to escape from
+   * the prompt.
+   *
+   * @param event The event to listen for.
+   */
   @EventHandler
   public void handleAsyncChatInput(AsyncPlayerChatEvent event) {
     Player player = event.getPlayer();
@@ -81,10 +96,15 @@ public class VersionedChatPrompt extends ChatPromptVersionTemplate {
       return;
     }
 
+    // cancel the event so that the prompt input is invisible for other players
     event.setCancelled(true);
     SimpleChatPrompt chatPrompt = this.chatPrompts.get(player.getUniqueId());
+
+    // if the message contains a % sign, handling the message will cause exceptions
+    // caused by bukkit. In order to display the % char correctly we have to replace it.
     String message = event.getMessage().replace("%", "%%");
 
+    // send echo if enabled
     if (chatPrompt.isEchoEnabled()) {
       StringBuilder echoMessage = new StringBuilder();
       if (chatPrompt.getEchoPrefix() != null) {
@@ -97,6 +117,8 @@ public class VersionedChatPrompt extends ChatPromptVersionTemplate {
       player.sendMessage(echoMessage.toString());
     }
 
+    // allow the player to escape from the prompt if they enter the
+    // exit flag.
     if (message.equalsIgnoreCase(chatPrompt.getCancelFlag())) {
       UUID taskId = this.playerTimeouts.get(player.getUniqueId()).getTaskId();
       this.schedulerRepository.interruptScheduler(taskId);
@@ -105,11 +127,18 @@ public class VersionedChatPrompt extends ChatPromptVersionTemplate {
       return;
     }
 
+    // let the prompt handler handle the input. Later the plugin checks whether
+    // the input was valid and delegates the player to another prompt.
+    // if not, the prompt will close.
     PromptResponseType responseType = chatPrompt.getResponseHandler().accept(message);
 
+    // no timeout will happen anymore as the response is already present.
+    // so timeout schedulers can be canceled. If the player is prompted
+    // again, the timeout will be started again automatically by this method.
     UUID taskId = this.playerTimeouts.get(player.getUniqueId()).getTaskId();
     this.schedulerRepository.interruptScheduler(taskId);
 
+    // if the handler stated the response is invalid, open the prompt again
     if (responseType == PromptResponseType.TRY_AGAIN) {
       this.simpleChatPrompt(chatPrompt);
       return;
@@ -119,6 +148,13 @@ public class VersionedChatPrompt extends ChatPromptVersionTemplate {
 
   }
 
+  /**
+   * If the player quits the server while being in a prompt, they
+   * have to get removed from the cache to avoid the plugin to
+   * wait for a response infinitely if no timeout is specified.
+   *
+   * @param event The event to listen for.
+   */
   @EventHandler
   public void handlePlayerQuit(PlayerQuitEvent event) {
     Player player = event.getPlayer();
@@ -127,12 +163,22 @@ public class VersionedChatPrompt extends ChatPromptVersionTemplate {
       return;
     }
 
+    // cancel timeout scheduler. If the scheduler refers to an offline player
+    // it will throw null pointer exceptions
     UUID taskId = this.playerTimeouts.get(player.getUniqueId()).getTaskId();
     this.schedulerRepository.interruptScheduler(taskId);
 
     this.removeFromCache(player.getUniqueId());
   }
 
+  /**
+   * Removes the chat prompt data from the given player from
+   * the cache. This data includes the timeout information as
+   * well as the chat prompt object (containing information about
+   * echo, cancel flag, etc.)
+   *
+   * @param player The player to be removed.
+   */
   private void removeFromCache(UUID player) {
     this.playerTimeouts.remove(player);
     this.chatPrompts.remove(player);
