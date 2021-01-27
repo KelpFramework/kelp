415a446e5619b092fc25b41492b7c0ac063af58e
pxav <pxav.studios@gmail.com>
2021-01-27 09:44:53 +0100

    Remove methods that are not needed anymore from sidebar version template

--- core/src/main/java/de/pxav/kelp/core/sidebar/version/SidebarVersionTemplate.java
+++ core/src/main/java/de/pxav/kelp/core/sidebar/version/SidebarVersionTemplate.java
@@ -16,39 +16,6 @@ import org.bukkit.scoreboard.Scoreboard;
 @KelpVersionTemplate
 public abstract class SidebarVersionTemplate {
 
-  /**
-   * @return A new, empty scoreboard.
-   */
-  public abstract Scoreboard createScoreboard();
-
-  /**
-   * Creates a scoreboard objective.
-   *
-   * @param parent The scoreboard in which the objective should be wrapped.
-   * @param identifier The name/identifier of the objective.
-   * @param title The display name/title of the objective.
-   * @return The final scoreboard objective.
-   */
-  public abstract Objective createObjective(Scoreboard parent,
-                                            String identifier,
-                                            String title);
-
-  /**
-   * Open the screboard for a player.
-   *
-   * @param scoreboard The actual scoreboard you want to open.
-   * @param player The player who should see the scoreboard.
-   */
-  public abstract void openScoreboard(Scoreboard scoreboard, Player player);
-
-  /**
-   * Close / hide the scoreboard for the player again.
-   *
-   * @param player The player whose scoreboard
-   *               should become invisible.
-   */
-  public abstract void closeScoreboard(Player player);
-
   public abstract void renderSidebar(KelpSidebar sidebar, KelpPlayer player);
 
   public abstract void lazyUpdate(KelpSidebar sidebar, KelpPlayer kelpPlayer);
