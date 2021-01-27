113f84318e7b30227e63143ee7ca38a05d3a9921
pxav <pxav.studios@gmail.com>
2021-01-27 21:28:18 +0100

    Add documentation for EmptyLineComponent

--- core/src/main/java/de/pxav/kelp/core/sidebar/component/EmptyLineComponent.java
+++ core/src/main/java/de/pxav/kelp/core/sidebar/component/EmptyLineComponent.java
@@ -12,8 +12,8 @@ import java.util.Map;
 
 /**
  * This sidebar component can be used to generate empty lines.
- * You could also write them manually as a {@code SimpleTextComponent},
- * but this way makes it more maintainable, because you don't
+ * You could also write them manually as a {@code StatelessTextComponent}
+ * for example, this way however makes it more maintainable, because you don't
  * have to choose color codes manually:
  *
  * To display an empty line in a scoreboard you have to use invisible
@@ -28,17 +28,38 @@ import java.util.Map;
  */
 public class EmptyLineComponent extends SidebarComponent {
 
+  // the line to place the component in the sidebar
   private int line;
 
   public static EmptyLineComponent create() {
     return new EmptyLineComponent();
   }
 
+  /**
+   * Sets the line number of the component to be placed in the sidebar.
+   * Please note that this line id represents an absolute position and
+   * should therefore be unique. No components should have the same line
+   * number.
+   *
+   * @param line The line of the sidebar, where the component should
+   *             be visible.
+   * @return The current component instance for fluent builder design.
+   */
   public EmptyLineComponent line(int line) {
     this.line = line;
     return this;
   }
 
+  /**
+   * Renders all the information provided in the component
+   * to a map containing the final information to be rendered
+   * to the sidebar (the lines where the text should be placed and
+   * the text to write there).
+   *
+   * @return A map, where the key is the absolute line where
+   *         the component should be placed in the sidebar and
+   *         the value is the actual text for that line.
+   */
   @Override
   public Map<Integer, String> render() {
     Map<Integer, String> output = Maps.newHashMap();
