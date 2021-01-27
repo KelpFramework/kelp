5c2c92e74f0e8a17326dc02b887729123cdf1094
pxav <pxav.studios@gmail.com>
2021-01-27 18:08:51 +0100

    Add documentation to LineSeparatorComponent

--- core/src/main/java/de/pxav/kelp/core/sidebar/component/LineSeparatorComponent.java
+++ core/src/main/java/de/pxav/kelp/core/sidebar/component/LineSeparatorComponent.java
@@ -42,26 +42,67 @@ public class LineSeparatorComponent extends SidebarComponent {
     return new LineSeparatorComponent();
   }
 
+  /**
+   * Sets the line number of the component to be placed in the sidebar.
+   * Please note that this line id represents an absolute position and
+   * should therefore be unique. No components should have the same line
+   * number.
+   *
+   * @param line The line of the sidebar, where the component should
+   *             be visible.
+   * @return The current component instance for fluent builder design.
+   */
   public LineSeparatorComponent line(int line) {
     this.line = line;
     return this;
   }
 
+  /**
+   * Sets the symbol to be repeated n times by the component, while
+   * {@code n} is equal to the length set by {@link #length(int)}
+   *
+   * @param symbol The symbol you want to set.
+   * @return Instance of the current component for more fluent builder design.
+   */
   public LineSeparatorComponent symbol(char symbol) {
     this.symbol = symbol;
     return this;
   }
 
+  /**
+   * The colors in which the line separator symbols should be displayed.
+   * You can also apply style codes such as {@link ChatColor#STRIKETHROUGH}
+   * here.
+   *
+   * @param colors The colors to be displayed.
+   * @return Instance of the current component for more fluent builder design.
+   */
   public LineSeparatorComponent color(ChatColor... colors) {
     this.colors = colors;
     return this;
   }
 
+  /**
+   * Sets the amount of times the given symbol should be repeated.
+   *
+   * @param length The length of your line separator.
+   * @return Instance of the current component for more fluent builder design.
+   */
   public LineSeparatorComponent length(int length) {
     this.length = length;
     return this;
   }
 
+  /**
+   * Renders all the information provided in the component
+   * to a map containing the final information to be rendered
+   * to the sidebar (the lines where the text should be placed and
+   * the text to write there).
+   *
+   * @return A map, where the key is the absolute line where
+   *         the component should be placed in the sidebar and
+   *         the value is the actual text for that line.
+   */
   @Override
   public Map<Integer, String> render() {
     Map<Integer, String> output = Maps.newHashMap();
@@ -79,6 +120,10 @@ public class LineSeparatorComponent extends SidebarComponent {
     return output;
   }
 
+  /**
+   * Contains some static values for possible length of
+   * a line separator.
+   */
   public static class SeparatorLength {
     public static final int FULL = 30;
     public static final int HALF = 15;
