c69ec235adb0f5264e74b8b70105b53553c73421
pxav <pxav.studios@gmail.com>
2021-01-27 15:00:05 +0100

    Adopt EmptyLineComponent to new component design

--- core/src/main/java/de/pxav/kelp/core/sidebar/component/EmptyLineComponent.java
+++ core/src/main/java/de/pxav/kelp/core/sidebar/component/EmptyLineComponent.java
@@ -1,10 +1,14 @@
 package de.pxav.kelp.core.sidebar.component;
 
+import com.google.common.collect.Maps;
+import de.pxav.kelp.core.KelpPlugin;
 import de.pxav.kelp.core.sidebar.SidebarUtils;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
+
+import java.util.Map;
 
 /**
  * This sidebar component can be used to generate empty lines.
@@ -22,34 +26,24 @@ import org.bukkit.scoreboard.Team;
  *
  * @author pxav
  */
-public class EmptyLineComponent implements SimpleSidebarComponent {
+public class EmptyLineComponent extends SidebarComponent {
 
   private int line;
 
-  private SidebarUtils sidebarUtils;
-
-  public EmptyLineComponent(SidebarUtils sidebarUtils) {
-    this.sidebarUtils = sidebarUtils;
+  public static EmptyLineComponent create() {
+    return new EmptyLineComponent();
   }
 
-  public EmptyLineComponent score(int line) {
+  public EmptyLineComponent line(int line) {
     this.line = line;
     return this;
   }
 
   @Override
-  public void render(Scoreboard parent) {
-    String entry = sidebarUtils.randomEmptyEntry(parent);
-    Objective objective = parent.getObjective(DisplaySlot.SIDEBAR);
-
-    objective.getScore(entry).setScore(line);
-
-    Team team = parent.registerNewTeam("entry_" + line);
-    team.addEntry(entry);
-    sidebarUtils.setTeamData(sidebarUtils.randomEmptyEntry(parent), team);
+  public Map<Integer, String> render() {
+    Map<Integer, String> output = Maps.newHashMap();
+    output.put(line, " ");
+    return output;
   }
-
-  @Override
-  public void update(Scoreboard parent) {}
 
 }
