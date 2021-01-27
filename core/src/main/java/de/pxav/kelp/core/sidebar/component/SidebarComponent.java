82b77985f693792207341a31dd7f8c97579c32ae
pxav <pxav.studios@gmail.com>
2021-01-27 09:56:19 +0100

    Add new abstract sidebar component class

---
+++ core/src/main/java/de/pxav/kelp/core/sidebar/component/SidebarComponent.java
@@ -0,0 +1,16 @@
+package de.pxav.kelp.core.sidebar.component;
+
+import java.util.Map;
+
+/**
+ * A class description goes here.
+ *
+ * @author pxav
+ */
+public abstract class SidebarComponent {
+
+  public abstract Map<Integer, String> render();
+
+  public abstract Map<Integer, String> update();
+
+}
