4d59910bfbc20b8f490605327177988bfc1ee36b
pxav <pxav.studios@gmail.com>
2021-01-27 09:44:18 +0100

    Add new updating and render methods to scoreboard version template

--- core/src/main/java/de/pxav/kelp/core/sidebar/version/SidebarVersionTemplate.java
+++ core/src/main/java/de/pxav/kelp/core/sidebar/version/SidebarVersionTemplate.java
@@ -1,20 +1,15 @@
 package de.pxav.kelp.core.sidebar.version;
 
 import de.pxav.kelp.core.application.KelpVersionTemplate;
+import de.pxav.kelp.core.player.KelpPlayer;
+import de.pxav.kelp.core.sidebar.component.SidebarComponent;
+import de.pxav.kelp.core.sidebar.type.AnimatedSidebar;
+import de.pxav.kelp.core.sidebar.type.KelpSidebar;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 
 /**
- * This basically is a template for version-specific implementations.
- * If you want to create a version specific implementation of scoreboards
- * you need to make your class inherit from this abstract class
- * and type the content of the given methods.
- *
- * Kelp always detects if there is an implementation installed and
- * automatically binds it to this class so that it can be injected
- * in your constructor. So you can always depend on this class if
- * you need version specific sidebar code.
  *
  * @author pxav
  */
@@ -53,5 +48,11 @@ public abstract class SidebarVersionTemplate {
    *               should become invisible.
    */
   public abstract void closeScoreboard(Player player);
+
+  public abstract void renderSidebar(KelpSidebar sidebar, KelpPlayer player);
+
+  public abstract void lazyUpdate(KelpSidebar sidebar, KelpPlayer kelpPlayer);
+
+  public abstract void updateSidebar(KelpSidebar sidebar, KelpPlayer player);
 
 }
