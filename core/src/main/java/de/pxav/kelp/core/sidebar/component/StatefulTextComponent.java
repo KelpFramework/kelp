5566531f39e9051998db7b7506c54693d684e6d9
pxav <pxav.studios@gmail.com>
2021-01-27 14:30:59 +0100

    Add StatefulTextComponent to handle dynamic texts in sidebars

---
+++ core/src/main/java/de/pxav/kelp/core/sidebar/component/StatefulTextComponent.java
@@ -0,0 +1,43 @@
+package de.pxav.kelp.core.sidebar.component;
+
+import com.google.common.collect.Maps;
+import de.pxav.kelp.core.scheduler.synchronize.Retrievable;
+
+import java.util.Map;
+
+/**
+ * A class description goes here.
+ *
+ * @author pxav
+ */
+public class StatefulTextComponent extends SidebarComponent {
+
+  private int line;
+  private Retrievable text;
+
+  public static StatefulTextComponent create() {
+    return new StatefulTextComponent();
+  }
+
+  public StatefulTextComponent line(int line) {
+    this.line = line;
+    return this;
+  }
+
+  public StatefulTextComponent text(Retrievable retrievableText) {
+    this.text = retrievableText;
+    return this;
+  }
+
+  @Override
+  public Map<Integer, String> render() {
+    Map<Integer, String> output = Maps.newHashMap();
+    output.put(line, String.valueOf(text.retrieve()));
+    return output;
+  }
+
+  @Override
+  public Map<Integer, String> update() {
+    return null;
+  }
+}
