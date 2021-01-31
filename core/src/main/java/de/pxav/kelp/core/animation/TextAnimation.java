2a42cdcb0b0702a504e74c90500da782bb883c43
pxav <pxav.studios@gmail.com>
2021-01-31 22:37:20 +0100

    Add documentation to text animation main interface

--- core/src/main/java/de/pxav/kelp/core/animation/TextAnimation.java
+++ core/src/main/java/de/pxav/kelp/core/animation/TextAnimation.java
@@ -3,12 +3,24 @@ package de.pxav.kelp.core.animation;
 import java.util.List;
 
 /**
- * A class description goes here.
+ * This interface represents the basis of every text animation
+ * used within the Kelp framework. A text animation basically takes
+ * a string and optionally some additional data and converts it to
+ * an animation. The animation consists of a collection of multiple
+ * strings. If you play those strings with a regular interval
+ * it will look like an animation.
  *
  * @author pxav
  */
 public interface TextAnimation {
 
+  /**
+   * This method calculates the given data from the animation class
+   * to a final animation. The list contains all animation states that - if played
+   * one by one - make up the final animation.
+   *
+   * @return The final collection of all states of the animation in a chronological order.
+   */
   List<String> states();
 
 }
