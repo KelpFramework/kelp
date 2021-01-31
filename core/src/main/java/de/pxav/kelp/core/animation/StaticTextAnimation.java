ae8f40a6405334f8417711747dbeeedf212e3f66
pxav <pxav.studios@gmail.com>
2021-01-31 22:37:39 +0100

    Add documentation to StaticTextAnimation

--- core/src/main/java/de/pxav/kelp/core/animation/StaticTextAnimation.java
+++ core/src/main/java/de/pxav/kelp/core/animation/StaticTextAnimation.java
@@ -4,7 +4,11 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * A class description goes here.
+ * This text animation is used to display static texts where a
+ * {@link TextAnimation} object is required by design. If you have
+ * an animated sidebar for example and you want to display a static
+ * text for example for debugging purposes, you can use this animation
+ * type.
  *
  * @author pxav
  */
@@ -22,6 +26,12 @@ public final class StaticTextAnimation implements TextAnimation {
     return new StaticTextAnimation();
   }
 
+  /**
+   * Sets the text to be displayed in the static animation.
+   *
+   * @param text The text to be displayed.
+   * @return Instance of the current animation object.
+   */
   public StaticTextAnimation text(String text) {
     this.text = text;
     return this;
