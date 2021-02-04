217969423770509d248f0094331fe8d63867620d
pxav <pxav.studios@gmail.com>
2021-02-04 11:51:11 +0100

    Bump plugin version to 0.1.1

--- core/src/main/java/de/pxav/kelp/core/KelpPlugin.java
+++ core/src/main/java/de/pxav/kelp/core/KelpPlugin.java
@@ -35,7 +35,7 @@ import java.util.logging.Level;
  *
  * @author pxav
  */
-@Plugin(name = "Kelp", version = "0.1.0")
+@Plugin(name = "Kelp", version = "0.1.1")
 @Author("pxav")
 @Description("A cross version spigot framework.")
 @Singleton
