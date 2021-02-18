64b9dbfb4e871df7a3e295535fdf92ddfab01d1c
pxav <pxav.studios@gmail.com>
2021-02-18 20:58:46 +0100

    Add wrapper class for bukkit chunks adding new practical chunk handling methods

---
+++ core/src/main/java/de/pxav/kelp/core/world/KelpChunk.java
@@ -0,0 +1,83 @@
+package de.pxav.kelp.core.world;
+
+import de.pxav.kelp.core.KelpPlugin;
+import org.bukkit.Chunk;
+
+public class KelpChunk {
+
+  private Chunk bukkitChunk;
+  private ChunkVersionTemplate versionTemplate;
+
+  KelpChunk(Chunk bukkitChunk, ChunkVersionTemplate versionTemplate) {
+    this.bukkitChunk = bukkitChunk;
+    this.versionTemplate = versionTemplate;
+  }
+
+  public static KelpChunk from(Chunk bukkitChunk) {
+    return new KelpChunk(
+      bukkitChunk,
+      KelpPlugin.getInjector().getInstance(ChunkVersionTemplate.class)
+    );
+  }
+
+  public KelpWorld getWorld() {
+    return versionTemplate.getWorld(this);
+  }
+
+  public int getX() {
+    return versionTemplate.getX(this);
+  }
+  public int getZ() {
+    return versionTemplate.getZ(this);
+  }
+
+  public KelpLocation getCenter(int height) {
+    return KelpLocation.from(getWorld().getName(), getX() << 4, height, getZ() << 4).add(7, 0, 7);
+  }
+
+  //todo rename methods! top left might also mean that its the y=256 block
+
+  public KelpBlock getTopLeftBlock(int height) {
+    KelpLocation location = KelpLocation.from(
+      getWorld().getName(),
+      getX() * 16 + 15,
+      height,
+      getZ() * 16
+    );
+    return getWorld().getBlockAt(location);
+  }
+
+  public KelpBlock getBottomLeftBlock(int height) {
+    KelpLocation location = KelpLocation.from(
+      getWorld().getName(),
+      getX() * 16,
+      height,
+      getZ() * 16
+    );
+    return getWorld().getBlockAt(location);
+  }
+
+  public KelpBlock getTopRightBlock(int height) {
+    KelpLocation location = KelpLocation.from(
+      getWorld().getName(),
+      getX() * 16 + 15,
+      height,
+      getZ() * 16 + 15
+    );
+    return getWorld().getBlockAt(location);
+  }
+
+  public KelpBlock getBottomRightBlock(int height) {
+    KelpLocation location = KelpLocation.from(
+      getWorld().getName(),
+      getX() * 16,
+      height,
+      getZ() * 16 + 15
+    );
+    return getWorld().getBlockAt(location);
+  }
+
+  public Chunk getBukkitChunk() {
+    return bukkitChunk;
+  }
+}
