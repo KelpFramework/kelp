548b99990ec7e40b924cbec12816f2e5caf6047f
pxav <pxav.studios@gmail.com>
2021-02-18 20:59:51 +0100

    Implement 1.8 version template for KelpLocation

---
+++ v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/world/VersionedLocation.java
@@ -0,0 +1,8 @@
+package de.pxav.kelp.implementation1_8.world;
+
+import de.pxav.kelp.core.version.Versioned;
+import de.pxav.kelp.core.world.LocationVersionTemplate;
+
+@Versioned
+public class VersionedLocation extends LocationVersionTemplate {
+}
