2c44511621cf7affaf9765261a013e2cb5dff8d3
pxav <pxav.studios@gmail.com>
2021-02-18 20:58:14 +0100

    Add block wrapper class used for version-independent block management

---
+++ core/src/main/java/de/pxav/kelp/core/world/KelpBlock.java
@@ -0,0 +1,40 @@
+package de.pxav.kelp.core.world;
+
+import de.pxav.kelp.core.KelpPlugin;
+import de.pxav.kelp.core.inventory.material.KelpMaterial;
+import org.bukkit.block.Block;
+
+public class KelpBlock {
+
+  private Block bukkitBlock;
+  private BlockVersionTemplate versionTemplate;
+
+  KelpBlock(Block bukkitBlock, BlockVersionTemplate versionTemplate) {
+    this.bukkitBlock = bukkitBlock;
+    this.versionTemplate = versionTemplate;
+  }
+
+  public static KelpBlock from(Block bukkitBlock) {
+    return new KelpBlock(
+      bukkitBlock,
+      KelpPlugin.getInjector().getInstance(BlockVersionTemplate.class)
+    );
+  }
+
+  public KelpChunk getChunk() {
+    return versionTemplate.getChunk(this);
+  }
+
+  public KelpMaterial getMaterial() {
+    return versionTemplate.getMaterial(this);
+  }
+
+  public void setMaterial(KelpMaterial material) {
+    versionTemplate.setMaterial(this, material);
+  }
+
+  public Block getBukkitBlock() {
+    return bukkitBlock;
+  }
+
+}
