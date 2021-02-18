d15f83b8ddfd8e08bd44cb87407031c08075e610
pxav <pxav.studios@gmail.com>
2021-02-18 20:50:03 +0100

    Add static factory for DroppedItemEntity

--- core/src/main/java/de/pxav/kelp/core/entity/type/DroppedItemEntity.java
+++ core/src/main/java/de/pxav/kelp/core/entity/type/DroppedItemEntity.java
@@ -1,10 +1,13 @@
 package de.pxav.kelp.core.entity.type;
 
+import de.pxav.kelp.core.KelpPlugin;
 import de.pxav.kelp.core.entity.KelpEntity;
 import de.pxav.kelp.core.entity.KelpEntityType;
+import de.pxav.kelp.core.entity.version.EntityTypeVersionTemplate;
 import de.pxav.kelp.core.entity.version.EntityVersionTemplate;
 import de.pxav.kelp.core.inventory.item.KelpItem;
 import org.bukkit.Location;
+import org.bukkit.entity.Item;
 
 /**
  * A class description goes here.
@@ -15,6 +18,10 @@ public class DroppedItemEntity extends KelpEntity {
 
   private KelpItem item;
   private ItemDropType itemDropType;
+
+  public static DroppedItemEntity from(Item item) {
+    return (DroppedItemEntity) KelpPlugin.getInjector().getInstance(EntityTypeVersionTemplate.class).getKelpEntity(item);
+  }
 
   public DroppedItemEntity() {}
 
