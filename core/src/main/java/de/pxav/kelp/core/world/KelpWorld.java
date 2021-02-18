04484e1bf10c8a4dcafb4f15941c57e24133bba3
pxav <pxav.studios@gmail.com>
2021-02-18 20:57:44 +0100

    Add KelpWorld class used for version-independent world management

---
+++ core/src/main/java/de/pxav/kelp/core/world/KelpWorld.java
@@ -0,0 +1,61 @@
+package de.pxav.kelp.core.world;
+
+import de.pxav.kelp.core.KelpPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+public class KelpWorld {
+
+  private String name;
+  private WorldType worldType;
+  private World bukkitWorld;
+
+  private WorldVersionTemplate versionTemplate;
+
+  public KelpWorld(World bukkitWorld, WorldVersionTemplate versionTemplate) {
+    this.bukkitWorld = bukkitWorld;
+    this.versionTemplate = versionTemplate;
+
+    this.name = bukkitWorld.getName();
+  }
+
+  public static KelpWorld from(String worldName) {
+    World world = Bukkit.getWorld(worldName);
+    if (world == null) {
+      return null;
+    }
+    return KelpWorld.from(world);
+  }
+
+  public static KelpWorld from(World world) {
+    return new KelpWorld(
+      world,
+      KelpPlugin.getInjector().getInstance(WorldVersionTemplate.class)
+    );
+  }
+
+  public World getBukkitWorld() {
+    return bukkitWorld;
+  }
+
+  public WorldType getWorldType() { //todo NULL!!
+    return worldType;
+  }
+
+  public KelpBlock getBlockAt(KelpLocation location) {
+    return versionTemplate.getBlockAt(this, location);
+  }
+
+  public KelpChunk getChunkAt(KelpLocation location) {
+    return versionTemplate.getChunkAt(this, location);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public KelpLocation getSpawnLocation() {
+    return versionTemplate.getSpawnLocation(this);
+  }
+
+}
