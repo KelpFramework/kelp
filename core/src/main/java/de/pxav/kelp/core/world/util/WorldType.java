f91a3e2492929dbd1919c8f194d93c33d12180c1
pxav <pxav.studios@gmail.com>
2021-02-24 14:37:30 +0100

    Provide documentation for WorldType

--- core/src/main/java/de/pxav/kelp/core/world/util/WorldType.java
+++ core/src/main/java/de/pxav/kelp/core/world/util/WorldType.java
@@ -1,9 +1,29 @@
 package de.pxav.kelp.core.world.util;
 
+/**
+ * Represents the generator type of a {@link de.pxav.kelp.core.world.KelpWorld}.
+ * This can be either a normal overworld or the nether or the end.
+ * This is the equivalent to the normal {@link org.bukkit.World.Environment} provided
+ * by bukkit.
+ *
+ * @author pxav
+ */
 public enum WorldType {
 
+  /**
+   * The current world is an overworld.
+   */
   NORMAL,
+
+  /**
+   * The current world is the end. This also changes the sky background
+   * to the purple background you might know.
+   */
   THE_END,
+
+  /**
+   * The current world is the nether.
+   */
   THE_NETHER
 
 }
