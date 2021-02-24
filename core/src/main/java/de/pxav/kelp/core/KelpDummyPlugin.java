9326a8e2098cfb304ed688b3be66edae98916605
pxav <pxav.studios@gmail.com>
2021-02-24 16:49:01 +0100

    Add documentation for KelpDummyPlugin

--- core/src/main/java/de/pxav/kelp/core/KelpDummyPlugin.java
+++ core/src/main/java/de/pxav/kelp/core/KelpDummyPlugin.java
@@ -2,4 +2,17 @@ package de.pxav.kelp.core;
 
 import de.pxav.kelp.core.application.KelpApplication;
 
+/**
+ * This class is used to pass a dummy plugin instance when developing
+ * inside the core module.
+ *
+ * For some things like {@link de.pxav.kelp.core.world.KelpChunk#addForceLoadFlag(Class)} you
+ * need to pass a {@link KelpApplication} class representing your plugin, but the kelp core
+ * obviously is no Kelp plugin, but a spigot plugin ({@link org.bukkit.plugin.java.JavaPlugin}).
+ *
+ * So if you ever run into such scenarios as a KelpCore developer, use this
+ * class.
+ *
+ * @author pxav
+ */
 public class KelpDummyPlugin extends KelpApplication {}
