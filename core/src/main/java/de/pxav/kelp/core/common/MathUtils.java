6d23a3f11bfca2db4d2c87585f9e8b5cef7f7a59
pxav <pxav.studios@gmail.com>
2021-02-27 20:34:39 +0100

    Add documentation to MathUtils

--- core/src/main/java/de/pxav/kelp/core/common/MathUtils.java
+++ core/src/main/java/de/pxav/kelp/core/common/MathUtils.java
@@ -2,16 +2,47 @@ package de.pxav.kelp.core.common;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * A small collection of some mathematical utils for Kelp.
+ * This class is functional, so all methods can be accessed statically.
+ *
+ * @author pxav
+ */
 public class MathUtils {
 
+  /**
+   * Checks if the given number x is even, which is
+   * equal to {@code x % 2 == 0}
+   *
+   * @param number The number to check.
+   * @return {@code true} if the number is even.
+   */
   public static boolean isEven(int number) {
     return number % 2 == 0;
   }
 
+  /**
+   * Checks if the given number x is odd, which is
+   * equal to {@code x % 2 == 1}
+   *
+   * @param number The number to check.
+   * @return {@code true} if the number is odd.
+   */
   public static boolean isOdd(int number) {
     return number % 2 != 0;
   }
 
+  /**
+   * Randomly calculates whether a certain chance has been
+   * fulfilled. If your {@code chance} is set to {@code 50},
+   * there will be a 50% chance that this method will return
+   * {@code true}. If it is {@code 5}, then there will be a
+   * {@code 5} per cent chance that this method will return
+   * {@code true}.
+   *
+   * @param chance The chance to use for random calculation.
+   * @return Either true or false depending on your luck and the provided chance.
+   */
   public static boolean perCentChance(int chance) {
     if (chance == 100) {
       return true;
