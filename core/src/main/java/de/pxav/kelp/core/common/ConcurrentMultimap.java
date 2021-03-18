9663ce9877e461f6696be6d5ea3971a4b149831d
pxav <pxav.studios@gmail.com>
2021-03-18 17:56:47 +0100

    Add custom interface for concurrent multimaps

---
+++ core/src/main/java/de/pxav/kelp/core/common/ConcurrentMultimap.java
@@ -0,0 +1,18 @@
+package de.pxav.kelp.core.common;
+
+import com.google.common.collect.Multimap;
+
+import java.util.Collection;
+import java.util.Map;
+
+public interface ConcurrentMultimap<K, V> extends Multimap<K, V> {
+
+  boolean containsValue(Collection<V> iterable);
+
+  Collection<V> getOrDefault(K key, Collection<V> defaultCollection);
+
+  Collection<V> getOrEmpty(K key);
+
+  void putAll(Map<K, V> newMap);
+
+}
