298c4c021666a64153ad0ab3f9a6eef6e4bcb8b9
pxav <pxav.studios@gmail.com>
2021-03-18 18:45:58 +0100

    Region repository can now trigger region leave and enter events

--- core/src/main/java/de/pxav/kelp/core/world/region/KelpRegionRepository.java
+++ core/src/main/java/de/pxav/kelp/core/world/region/KelpRegionRepository.java
@@ -1,55 +1,84 @@
 package de.pxav.kelp.core.world.region;
 
-import com.google.common.collect.*;
+import de.pxav.kelp.core.common.ConcurrentMultimap;
+import de.pxav.kelp.core.common.ConcurrentSetMultimap;
+import de.pxav.kelp.core.event.kelpevent.PlayerEnterRegionEvent;
+import de.pxav.kelp.core.event.kelpevent.PlayerLeaveRegionEvent;
+import de.pxav.kelp.core.player.KelpPlayer;
 import de.pxav.kelp.core.scheduler.KelpSchedulerRepository;
 import de.pxav.kelp.core.scheduler.type.RepeatingScheduler;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.Map;
 import java.util.UUID;
 
 @Singleton
 public class KelpRegionRepository {
 
-  // region id -> region object
-  private Map<UUID, KelpRegion> listenedRegions;
+  private ConcurrentSetMultimap<ApproximateLocation, KelpRegion> nearRegions;
 
   // player -> region id
-  private Multimap<UUID, UUID> containedBy;
+  private ConcurrentMultimap<UUID, UUID> containedBy;
 
-  private boolean listenerRunning = false;
+  private UUID task;
 
   private KelpSchedulerRepository schedulerRepository;
 
   @Inject
   public KelpRegionRepository(KelpSchedulerRepository schedulerRepository) {
-    this.listenedRegions = Maps.newConcurrentMap();
-    this.containedBy = ArrayListMultimap.create();
+    this.containedBy = ConcurrentSetMultimap.create();
+    this.nearRegions = ConcurrentSetMultimap.create();
 
     this.schedulerRepository = schedulerRepository;
   }
 
   public void listenTo(KelpRegion region) {
-    this.listenedRegions.put(region.getRegionId(), region);
-    if (!listenerRunning) {
+    ApproximateLocation maxPos = ApproximateLocation.from(region.getMaxPos());
+    ApproximateLocation minPos = ApproximateLocation.from(region.getMinPos());
+
+    for (int x = minPos.getX(); x <= maxPos.getX(); x++) {
+      for (int z = minPos.getZ(); z <= maxPos.getZ(); z++) {
+        ApproximateLocation current = ApproximateLocation.create(maxPos.getWorldName(), x, z);
+        nearRegions.put(current, region);
+      }
+    }
+
+    if (!isListenerRunning()) {
       startListenerTasks();
     }
   }
 
   public void stopListeningTo(KelpRegion region) {
-    this.listenedRegions.remove(region.getRegionId());
+    ApproximateLocation maxPos = ApproximateLocation.from(region.getMaxPos());
+    ApproximateLocation minPos = ApproximateLocation.from(region.getMinPos());
 
+    for (int x = minPos.getX(); x <= maxPos.getX(); x++) {
+      for (int z = minPos.getZ(); z <= maxPos.getZ(); z++) {
+        ApproximateLocation current = ApproximateLocation.create(maxPos.getWorldName(), x, z);
+        nearRegions.remove(current, region);
+      }
+    }
   }
 
   public boolean isListeningTo(KelpRegion region) {
-    return this.listenedRegions.containsKey(region.getRegionId());
+    return nearRegions.containsValue(region);
   }
 
-  public void startListenerTasks() {
-    RepeatingScheduler.create()
+  @EventHandler
+  public void handlePlayerJoin(PlayerJoinEvent event) {
+    if (nearRegions.isEmpty()) {
+      return;
+    }
+
+    this.startListenerTasks();
+  }
+
+  private void startListenerTasks() {
+    task = RepeatingScheduler.create()
       .async()
       .every(100)
       .milliseconds()
@@ -57,24 +86,43 @@ public class KelpRegionRepository {
       .run(taskId -> {
 
         // if there are no regions to listen to anymore, cancel the scheduler
-        if (listenedRegions.isEmpty()) {
-          listenerRunning = false;
-          schedulerRepository.interruptScheduler(taskId);
+        if (nearRegions.isEmpty() || Bukkit.getOnlinePlayers().size() == 0) {
+          stopListenerTasks();
         }
 
-        listenerRunning = true;
-
         for (Player player : Bukkit.getOnlinePlayers()) {
-          listenedRegions.forEach((regionId, region) -> {
+          nearRegions.getOrEmpty(ApproximateLocation.from(player.getLocation())).forEach(region -> {
             if (!region.getWorldName().equalsIgnoreCase(player.getWorld().getName())) {
               return;
             }
 
+            boolean contains = region.contains(
+              player.getLocation().getX(),
+              player.getLocation().getY(),
+              player.getLocation().getZ()
+            );
 
+            if (contains && !containedBy.containsEntry(player.getUniqueId(), region.getRegionId())) {
+              this.containedBy.put(player.getUniqueId(), region.getRegionId());
+              Bukkit.getPluginManager().callEvent(new PlayerEnterRegionEvent(region, KelpPlayer.from(player)));
+            } else if (!contains && containedBy.containsEntry(player.getUniqueId(), region.getRegionId())) {
+              this.containedBy.remove(player.getUniqueId(), region.getRegionId());
+              Bukkit.getPluginManager().callEvent(new PlayerLeaveRegionEvent(region, KelpPlayer.from(player)));
+            }
           });
         }
     });
   }
 
+  private void stopListenerTasks() {
+    if (task != null) {
+      schedulerRepository.interruptScheduler(task);
+      task = null;
+    }
+  }
+
+  private boolean isListenerRunning() {
+    return task != null;
+  }
 
 }
