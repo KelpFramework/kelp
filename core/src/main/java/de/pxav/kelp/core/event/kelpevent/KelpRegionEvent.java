180592b495c08e84c356e147101e5f155c7a9abe
pxav <pxav.studios@gmail.com>
2021-03-19 21:55:30 +0100

    Add documentation for KelpRegionEvent

--- core/src/main/java/de/pxav/kelp/core/event/kelpevent/KelpRegionEvent.java
+++ core/src/main/java/de/pxav/kelp/core/event/kelpevent/KelpRegionEvent.java
@@ -3,14 +3,28 @@ package de.pxav.kelp.core.event.kelpevent;
 import de.pxav.kelp.core.world.region.KelpRegion;
 import org.bukkit.event.Event;
 
+/**
+ * A region event is any event that is related to a specific {@link KelpRegion}.
+ *
+ * In most cases you first have to enable listeners for your region using
+ * {@link KelpRegion#enableListeners()} to work with this region.
+ *
+ * @author pxav
+ */
 public abstract class KelpRegionEvent extends Event {
 
+  // the region handled by this event
   protected KelpRegion region;
 
   public KelpRegionEvent(KelpRegion region) {
     this.region = region;
   }
 
+  /**
+   * Gets the region involved in the current event.
+   *
+   * @return The region involved in this event.
+   */
   public KelpRegion getRegion() {
     return region;
   }
