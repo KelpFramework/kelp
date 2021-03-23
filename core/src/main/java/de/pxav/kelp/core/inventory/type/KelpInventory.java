3915155604e4ecdce636e41062aaceb67aaa2107
pxav <pxav.studios@gmail.com>
2021-03-23 17:54:54 +0100

    KelpInventory base class now holds all inventory widgets instead of the implementations such as AnimatedInventory

--- core/src/main/java/de/pxav/kelp/core/inventory/type/KelpInventory.java
+++ core/src/main/java/de/pxav/kelp/core/inventory/type/KelpInventory.java
@@ -1,7 +1,13 @@
 package de.pxav.kelp.core.inventory.type;
 
+import com.google.common.collect.Lists;
+import de.pxav.kelp.core.inventory.widget.GroupedWidget;
+import de.pxav.kelp.core.inventory.widget.SimpleWidget;
+import de.pxav.kelp.core.inventory.widget.Widget;
 import de.pxav.kelp.core.player.KelpPlayer;
 import org.bukkit.inventory.Inventory;
+
+import java.util.List;
 
 /**
  * A class description goes here.
@@ -10,8 +16,20 @@ import org.bukkit.inventory.Inventory;
  */
 public abstract class KelpInventory {
 
+  protected int size;
+  protected List<SimpleWidget> simpleWidgets = Lists.newArrayList();
+  protected List<GroupedWidget> groupedWidgets = Lists.newArrayList();
+
   public abstract Inventory render(KelpPlayer player);
 
   public abstract void update(KelpPlayer toUpdate);
+
+  public final void removeWidgetsAndClose() {
+    onClose();
+    simpleWidgets.forEach(Widget::onRemove);
+    groupedWidgets.forEach(Widget::onRemove);
+  }
+
+  public void onClose() {}
 
 }
