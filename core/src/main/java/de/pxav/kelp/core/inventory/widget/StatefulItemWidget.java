82c16396d0397be3cb580a227a1229196ce2c40c
pxav <pxav.studios@gmail.com>
2021-03-26 13:12:33 +0100

    Add new widget displaying a stateful item: StatefulItemWidget

---
+++ core/src/main/java/de/pxav/kelp/core/inventory/widget/StatefulItemWidget.java
@@ -0,0 +1,60 @@
+package de.pxav.kelp.core.inventory.widget;
+
+import com.google.common.collect.Sets;
+import de.pxav.kelp.core.inventory.item.KelpItem;
+import de.pxav.kelp.core.inventory.listener.KelpClickEvent;
+
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class StatefulItemWidget extends AbstractWidget<StatefulItemWidget> implements SimpleWidget {
+
+  private Supplier<KelpItem> itemSupplier;
+
+  // caches listeners to be added to the item, when the item itself is still null.
+  private Set<Consumer<KelpClickEvent>> listenerCache;
+
+  public static StatefulItemWidget create() {
+    return new StatefulItemWidget();
+  }
+
+  StatefulItemWidget() {
+    listenerCache = Sets.newHashSet();
+  }
+
+  public StatefulItemWidget item(Supplier<KelpItem> itemSupplier) {
+    this.itemSupplier = itemSupplier;
+    return this;
+  }
+
+  public StatefulItemWidget addItemListener(Consumer<KelpClickEvent> listener) {
+    if (listenerCache == null) {
+      listenerCache = Sets.newHashSet();
+    }
+
+    listenerCache.add(listener);
+    return this;
+  }
+
+  @Override
+  public KelpItem render() {
+    // add cached listeners which were created when then item
+    // did not exist.
+    KelpItem item = itemSupplier.get();
+    listenerCache.forEach(listener -> addClickListener(item, listener));
+
+    return item;
+  }
+
+  @Override
+  public int getCoveredSlot() {
+    return itemSupplier.get().getSlot();
+  }
+
+  @Override
+  public boolean isStateful() {
+    return true;
+  }
+
+}
