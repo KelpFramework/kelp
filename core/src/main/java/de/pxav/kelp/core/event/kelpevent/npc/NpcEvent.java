ac4dacd7331eb50d780e9c319aec1009b1cc9ef5
pxav <pxav.studios@gmail.com>
2021-03-30 13:50:06 +0200

    Add documentation to NpcEvent base

--- core/src/main/java/de/pxav/kelp/core/event/kelpevent/npc/NpcEvent.java
+++ core/src/main/java/de/pxav/kelp/core/event/kelpevent/npc/NpcEvent.java
@@ -5,22 +5,34 @@ import de.pxav.kelp.core.player.KelpPlayer;
 import org.bukkit.event.Event;
 
 /**
- * A class description goes here.
+ * Represents an event that is related to a {@link KelpNpc}.
  *
  * @author pxav
  */
 public abstract class NpcEvent extends Event {
 
+  // the npc handled by this event
   protected KelpNpc npc;
 
   public NpcEvent(KelpNpc npc) {
     this.npc = npc;
   }
 
+  /**
+   * Gets the {@link KelpNpc NPC} that is handled by this event.
+   *
+   * @return The npc that is handled by this event.
+   */
   public final KelpNpc getNpc() {
     return this.npc;
   }
 
+  /**
+   * Gets the owner of the NPC, the player who can see the NPC
+   * or is associated with it in the server cache.
+   *
+   * @return the owning {@link KelpPlayer player} of the NPC.
+   */
   public final KelpPlayer getPlayer() {
     return npc.getPlayer();
   }
