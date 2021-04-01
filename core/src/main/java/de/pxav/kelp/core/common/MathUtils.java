a43653522c042ff877a6e7f02c172782fc2418c0
pxav <pxav.studios@gmail.com>
2021-04-01 11:53:00 +0200

    You can now get roman number from a decimal integer

--- core/src/main/java/de/pxav/kelp/core/common/MathUtils.java
+++ core/src/main/java/de/pxav/kelp/core/common/MathUtils.java
@@ -10,6 +10,9 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class MathUtils {
 
+  private static final int[] values = {           1000, 900,  500, 400,  100, 90,   50,  40,   10,  9,    5,   4,    1 };
+  private static final String[] romanLiterals = { "M",  "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
   /**
    * Checks if the given number x is even, which is
    * equal to {@code x % 2 == 0}
@@ -51,6 +54,18 @@ public class MathUtils {
       return false;
     }
     return ThreadLocalRandom.current().nextInt(0, 101) < chance;
+  }
+
+  public static String getRomanNumber(int number) {
+    StringBuilder s = new StringBuilder();
+
+    for (int i = 0; i < values.length; i++) {
+      while (number >= values[i]) {
+        number -= values[i];
+        s.append(romanLiterals[i]);
+      }
+    }
+    return s.toString();
   }
 
 }
