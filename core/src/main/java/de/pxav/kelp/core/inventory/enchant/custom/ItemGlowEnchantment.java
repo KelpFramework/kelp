c6bf82b2fe04c3cf13241a6e85e3a2da15488fcd
pxav <pxav.studios@gmail.com>
2021-04-02 11:46:20 +0200

    Add ItemGlowEnchantment allowing you to create a glow effect on items without actually enchanting them

---
+++ core/src/main/java/de/pxav/kelp/core/inventory/enchant/custom/ItemGlowEnchantment.java
@@ -0,0 +1,56 @@
+package de.pxav.kelp.core.inventory.enchant.custom;
+
+import de.pxav.kelp.core.inventory.enchant.KelpEnchantment;
+import de.pxav.kelp.core.inventory.item.KelpItem;
+import de.pxav.kelp.core.inventory.material.KelpMaterial;
+
+/**
+ * This enchantment can be applied to an item if you want to make it
+ * glow without giving it a special ability or showing the enchantment
+ * in the item lore/interfering with "real" enchantments.
+ *
+ * This enchantment can be applied to any item and can be combined with
+ * any other enchantment safely. It won't be added to the item's lore.
+ *
+ * You can directly use the glow effect via the shortcuts provided by
+ * the kelp item class:
+ * - {@link KelpItem#addGlow()}
+ * - {@link KelpItem#removeGlow()}
+ * - {@link KelpItem#setGlowing(boolean)}
+ * - {@link KelpItem#hasGlow()}
+ *
+ * @author pxav
+ */
+public class ItemGlowEnchantment extends KelpEnchantment {
+
+  @Override
+  public String getName() {
+    return "ItemGlow";
+  }
+
+  @Override
+  public int getStartLevel() {
+    return 1;
+  }
+
+  @Override
+  public int getMaxLevel() {
+    return 1;
+  }
+
+  @Override
+  public boolean canEnchant(KelpMaterial material) {
+    return true;
+  }
+
+  @Override
+  public boolean conflictsWith(String enchantmentName) {
+    return false;
+  }
+
+  @Override
+  public boolean addToItemDescription() {
+    return false;
+  }
+
+}
