e4006364d739964d1248be102c16b3c4c12059ab
pxav <pxav.studios@gmail.com>
2021-04-15 20:19:31 +0200

    LivingKelpEntity now is an independent interface is well

--- core/src/main/java/de/pxav/kelp/core/entity/LivingKelpEntity.java
+++ core/src/main/java/de/pxav/kelp/core/entity/LivingKelpEntity.java
@@ -1,51 +1,18 @@
 package de.pxav.kelp.core.entity;
 
-import de.pxav.kelp.core.entity.version.EntityVersionTemplate;
-import de.pxav.kelp.core.entity.version.LivingEntityVersionTemplate;
+import de.pxav.kelp.core.entity.type.general.DamageableEntity;
 import de.pxav.kelp.core.world.KelpLocation;
-import org.bukkit.Location;
-import org.bukkit.entity.LivingEntity;
 
-/**
- * A class description goes here.
- *
- * @author pxav
- */
-public class LivingKelpEntity extends KelpEntity {
+public interface LivingKelpEntity<T extends LivingKelpEntity<?>> extends KelpEntity<T>, DamageableEntity<T> {
 
-  protected LivingEntityVersionTemplate livingEntityVersionTemplate;
-  protected LivingEntity bukkitLivingEntity;
-
-  public LivingKelpEntity(Object minecraftEntity,
-                          KelpEntityType entityType,
-                          Location initialLocation,
-                          int entityId,
-                          EntityVersionTemplate entityVersionTemplate,
-                          LivingEntityVersionTemplate livingEntityVersionTemplate,
-                          LivingEntity bukkitLivingEntity) {
-    super(minecraftEntity, entityType, initialLocation, entityId, entityVersionTemplate);
-    this.livingEntityVersionTemplate = livingEntityVersionTemplate;
-    this.bukkitLivingEntity = bukkitLivingEntity;
-  }
-
-  public LivingKelpEntity() {}
-
-  public LivingKelpEntity bukkitLivingEntity(LivingEntity bukkitLivingEntity) {
-    this.bukkitLivingEntity = bukkitLivingEntity;
-    return this;
-  }
-
-  public LivingKelpEntity livingEntityVersionTemplate(LivingEntityVersionTemplate livingEntityVersionTemplate) {
-    this.livingEntityVersionTemplate = livingEntityVersionTemplate;
-    return this;
-  }
-
-  public LivingEntity getBukkitLivingEntity() {
-    return bukkitLivingEntity;
-  }
-
-  public KelpLocation getEyeLocation() {
-    return this.livingEntityVersionTemplate.getEyeLocation(bukkitLivingEntity);
-  }
+  /**
+   * Gets the location of the entity's eyes. When you get
+   * the normal location of a player for example, the feet location
+   * will be returned. This method gets the eye location depending
+   * on the current entity type.
+   *
+   * @return The location of the entity's eyes.
+   */
+  KelpLocation getEyeLocation();
 
 }
