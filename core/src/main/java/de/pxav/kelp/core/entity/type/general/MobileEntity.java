34291f79660401085d44c726230a9fd8cdc84c36
pxav <pxav.studios@gmail.com>
2021-04-15 20:20:22 +0200

    Add superinterface for Mobs (mobile entities)

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/general/MobileEntity.java
@@ -0,0 +1,15 @@
+package de.pxav.kelp.core.entity.type.general;
+
+import de.pxav.kelp.core.entity.LivingKelpEntity;
+
+public interface MobileEntity<T extends MobileEntity<?>> extends LivingKelpEntity<T> {
+
+  T setTarget(LivingKelpEntity<?> target);
+
+  LivingKelpEntity<?> getTarget();
+
+  boolean isAware();
+
+  boolean setAware(boolean aware);
+
+}
