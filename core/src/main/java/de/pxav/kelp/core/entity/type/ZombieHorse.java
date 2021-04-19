76b639982fd6c300a2aface58f7f9af9d0792bec
pxav <pxav.studios@gmail.com>
2021-04-19 19:46:07 +0200

    Add interface for zombie horses

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/ZombieHorse.java
@@ -0,0 +1,7 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.AbstractHorseEntity;
+
+public interface ZombieHorse extends AbstractHorseEntity<ZombieHorse> {
+
+}
