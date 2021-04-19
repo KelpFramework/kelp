abb828cdb78b365048764d37a3ce5c7a97e88c1b
pxav <pxav.studios@gmail.com>
2021-04-19 19:56:19 +0200

    Add interface for evokers

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/EvokerEntity.java
@@ -0,0 +1,7 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.SpellcastingEntity;
+
+public interface EvokerEntity extends SpellcastingEntity<EvokerEntity> {
+
+}
