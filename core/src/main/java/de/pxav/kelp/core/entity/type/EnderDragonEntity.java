ad1c66f8341d236af63c701867b8dc5008d6bc3a
pxav <pxav.studios@gmail.com>
2021-04-19 19:55:31 +0200

    Add interface for ender dragons

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/EnderDragonEntity.java
@@ -0,0 +1,11 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.LivingKelpEntity;
+
+public interface EnderDragonEntity extends LivingKelpEntity<EnderDragonEntity> {
+
+  int getDeathAnimationInTicks();
+
+  // TODO implement other ender dragon specific things
+
+}
