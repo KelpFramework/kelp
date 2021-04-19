3d7653a17ca437022978e49a77fcc6471e5cce21
pxav <pxav.studios@gmail.com>
2021-04-19 19:53:42 +0200

    Add interface for cod (fish)

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/CodEntity.java
@@ -0,0 +1,8 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.AbstractFishEntity;
+import org.bukkit.entity.Cod;
+
+public interface CodEntity extends AbstractFishEntity<CodEntity> {
+
+}
