4fedf6a4b0ffb815b8ad69a021061f5e9ba7c918
pxav <pxav.studios@gmail.com>
2021-04-19 19:59:55 +0200

    Add specific methods to AnimalEntity as there is now reverse-engineered data for those values

--- core/src/main/java/de/pxav/kelp/core/entity/type/general/AnimalEntity.java
+++ core/src/main/java/de/pxav/kelp/core/entity/type/general/AnimalEntity.java
@@ -1,5 +1,19 @@
 package de.pxav.kelp.core.entity.type.general;
 
+import de.pxav.kelp.core.entity.KelpEntity;
+
 public interface AnimalEntity<T extends AnimalEntity<?>> extends MobileEntity<T> {
+
+    boolean isInLove();
+
+    T setInLove(boolean inLove);
+
+    KelpEntity<?> getBreeder();
+
+    T setBreeder(KelpEntity<?> breeder);
+
+    int getLoveModeTicks();
+
+    T setLoveModeTicks(int loveModeTicks);
 
 }
