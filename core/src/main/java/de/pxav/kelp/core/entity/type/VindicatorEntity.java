d31888c2ab88dfe3f82fabcfcd0e7a1eb2505d33
pxav <pxav.studios@gmail.com>
2021-04-19 19:47:47 +0200

    Add interface for vindicators

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/VindicatorEntity.java
@@ -0,0 +1,7 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.IllagerEntity;
+
+public interface VindicatorEntity extends IllagerEntity<VindicatorEntity> {
+
+}
