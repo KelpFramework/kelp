7e78de87f8aa165f5226f7803d5f2de8c2f2b270
pxav <pxav.studios@gmail.com>
2021-04-19 14:14:02 +0200

    Add default values for the properties in MonsterEntity

--- core/src/main/java/de/pxav/kelp/core/entity/type/general/MonsterEntity.java
+++ core/src/main/java/de/pxav/kelp/core/entity/type/general/MonsterEntity.java
@@ -3,21 +3,27 @@ package de.pxav.kelp.core.entity.type.general;
 import org.bukkit.Bukkit;
 import org.bukkit.Difficulty;
 import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftCreature;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftMob;
 
 public interface MonsterEntity<T extends MonsterEntity<?>> extends MobileEntity<T> {
 
-  int getMaximumSpawnLightLevel();
+  default int getMaximumSpawnLightLevel() {
+    return 0;
+  }
 
   default boolean canSpawnInLightLevel(int lightLevel) {
     return getMaximumSpawnLightLevel() >= lightLevel;
   }
 
-  int getAttackDamage(Difficulty difficulty);
+  default double getAttackDamage(Difficulty difficulty) {
+    return 0;
+  }
 
-  default int getAttackDamage() {
+  default double getAttackDamage() {
     World world = Bukkit.getWorld(getLocation().getWorldName());
     if (world == null) {
-      return 0;
+      return 0.0d;
     }
     return getAttackDamage(world.getDifficulty());
   }
