8d8c9e31d334a824be6c47ffe20bf545a7c83947
pxav <pxav.studios@gmail.com>
2021-04-19 20:03:09 +0200

    Implement abstract water entities

---
+++ v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/entity/type/general/VersionedWaterEntity.java
@@ -0,0 +1,15 @@
+package de.pxav.kelp.implementation1_8.entity.type.general;
+
+import de.pxav.kelp.core.entity.KelpEntityType;
+import de.pxav.kelp.core.entity.type.general.WaterEntity;
+import de.pxav.kelp.core.entity.version.EntityTypeVersionTemplate;
+import net.minecraft.server.v1_8_R3.Entity;
+import org.bukkit.Location;
+
+public class VersionedWaterEntity<T extends WaterEntity<T>> extends VersionedMobileEntity<T> implements WaterEntity<T> {
+
+  public VersionedWaterEntity(Entity entityHandle, KelpEntityType entityType, Location initialLocation, EntityTypeVersionTemplate entityTypeVersionTemplate) {
+    super(entityHandle, entityType, initialLocation, entityTypeVersionTemplate);
+  }
+
+}
