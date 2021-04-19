2583a836e5c239a6c464cdd597a6197d11bbf86d
pxav <pxav.studios@gmail.com>
2021-04-19 20:01:18 +0200

    Implement AI methods to Mobile entity

--- v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/entity/type/general/VersionedMobileEntity.java
+++ v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/entity/type/general/VersionedMobileEntity.java
@@ -3,44 +3,57 @@ package de.pxav.kelp.implementation1_8.entity.type.general;
 import de.pxav.kelp.core.entity.KelpEntityType;
 import de.pxav.kelp.core.entity.LivingKelpEntity;
 import de.pxav.kelp.core.entity.type.general.MobileEntity;
+import de.pxav.kelp.core.entity.version.EntityTypeVersionTemplate;
 import de.pxav.kelp.core.world.KelpLocation;
 import de.pxav.kelp.implementation1_8.entity.VersionedLivingEntity;
 import net.minecraft.server.v1_8_R3.Entity;
+import net.minecraft.server.v1_8_R3.EntityCreature;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftCreature;
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.LivingEntity;
 
+/**
+ * There is currently no implementation of a {@code Mob} in craftbukkit, so
+ * this mob implementation uses the {@code Creature} implementation provided
+ * by bukkit and NMS.
+ *
+ * @param <T>
+ */
 public class VersionedMobileEntity<T extends MobileEntity<T>>
   extends VersionedLivingEntity<T>
   implements MobileEntity<T> {
 
+  private EntityCreature creatureHandle;
+  private CraftCreature craftCreature;
+
   public VersionedMobileEntity(Entity entityHandle,
                                KelpEntityType entityType,
-                               Location initialLocation) {
-    super(entityHandle, entityType, initialLocation);
+                               Location initialLocation, EntityTypeVersionTemplate entityTypeVersionTemplate) {
+    super(entityHandle, entityType, initialLocation, entityTypeVersionTemplate);
+    this.craftCreature = (CraftCreature) entityHandle.getBukkitEntity();
+    this.creatureHandle = (EntityCreature) entityHandle;
   }
 
   @Override
   public T setTarget(LivingKelpEntity<?> target) {
-    return null;
+    craftCreature.setTarget((LivingEntity) target.getBukkitEntity());
+    return (T) this;
   }
 
   @Override
   public LivingKelpEntity<?> getTarget() {
-    return null;
+    return (LivingKelpEntity<?>) entityTypeVersionTemplate.getKelpEntity(craftCreature.getTarget());
   }
 
   @Override
   public boolean isAware() {
-    return false;
+    return craftCreature.getTarget() != null;
   }
 
   @Override
-  public boolean setAware(boolean aware) {
-    return false;
-  }
-
-  @Override
-  public KelpLocation getEyeLocation() {
-    return null;
+  public T setAware(boolean aware) {
+    return (T) this;
   }
 
 }
