052c956cc5c7c48a984319ab727f5b37542d6222
pxav <pxav.studios@gmail.com>
2021-04-19 19:50:42 +0200

    Add interface for thrown tridents

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/ThrownTridentEntity.java
@@ -0,0 +1,7 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.AbstractArrowEntity;
+
+public interface ThrownTridentEntity extends AbstractArrowEntity<ThrownTridentEntity> {
+
+}
