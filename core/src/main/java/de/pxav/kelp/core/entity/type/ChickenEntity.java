d1b1084cf135fca44450d515df229fcd90dfe7fd
pxav <pxav.studios@gmail.com>
2021-04-19 19:53:32 +0200

    Add interface for chicken

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/ChickenEntity.java
@@ -0,0 +1,6 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.BreedableAnimalEntity;
+
+public interface ChickenEntity extends BreedableAnimalEntity<ChickenEntity> {
+}
