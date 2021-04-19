beabb09ad6620752c9db7a1c874570fa8a0e214f
pxav <pxav.studios@gmail.com>
2021-04-19 19:56:44 +0200

    Add interface for hopper minecarts

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/HopperMinecart.java
@@ -0,0 +1,11 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.MinecartEntity;
+
+public interface HopperMinecart extends MinecartEntity<HopperMinecart> {
+
+  boolean isHopperEnabled();
+
+  HopperMinecart setHopperEnabled();
+
+}
