2e32ffb37cabf4a568b0522feda341344557f6f8
pxav <pxav.studios@gmail.com>
2021-04-19 19:50:35 +0200

    Add interface for the skull thrown by withers

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/ThrownWitherSkullEntity.java
@@ -0,0 +1,9 @@
+package de.pxav.kelp.core.entity.type;
+
+public interface ThrownWitherSkullEntity extends ThrownFireballEntity {
+
+  boolean isCharged();
+
+  ThrownWitherSkullEntity setCharged(boolean charged);
+
+}
