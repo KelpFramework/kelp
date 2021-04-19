8b54f43176f9200c5df7369eb9aab151414c22c5
pxav <pxav.studios@gmail.com>
2021-04-19 19:50:55 +0200

    Add interface for thrown snowballs

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/ThrownSnowballEntity.java
@@ -0,0 +1,7 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.KelpProjectile;
+
+public interface ThrownSnowballEntity extends KelpProjectile<ThrownSnowballEntity> {
+
+}
