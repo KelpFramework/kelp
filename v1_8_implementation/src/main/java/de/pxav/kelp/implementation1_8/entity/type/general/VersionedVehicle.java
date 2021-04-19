c825daeed92342e977c482d7a47ee3318b6e1bff
pxav <pxav.studios@gmail.com>
2021-04-19 12:45:37 +0200

    Implement Vehicle Entity

---
+++ v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/entity/type/general/VersionedVehicle.java
@@ -0,0 +1,27 @@
+package de.pxav.kelp.implementation1_8.entity.type.general;
+
+import de.pxav.kelp.core.entity.KelpEntityType;
+import de.pxav.kelp.core.entity.type.general.VehicleEntity;
+import de.pxav.kelp.core.entity.version.EntityTypeVersionTemplate;
+import de.pxav.kelp.implementation1_8.entity.VersionedEntity;
+import net.minecraft.server.v1_8_R3.Entity;
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+public class VersionedVehicle<T extends VehicleEntity<T>> extends VersionedEntity<T> implements VehicleEntity<T> {
+
+  public VersionedVehicle(Entity entityHandle, KelpEntityType entityType, Location initialLocation, EntityTypeVersionTemplate entityTypeVersionTemplate) {
+    super(entityHandle, entityType, initialLocation, entityTypeVersionTemplate);
+  }
+
+  @Override
+  public Vector getVehicleVelocity() {
+    return getVelocity();
+  }
+
+  @Override
+  public T setVehicleVelocity(Vector vehicleVelocity) {
+    return setVelocity(vehicleVelocity);
+  }
+
+}
