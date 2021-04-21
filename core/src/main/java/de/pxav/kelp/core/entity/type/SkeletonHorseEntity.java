1a18ced88c8c7ce3779192d95f430ae7662976ee
pxav <pxav.studios@gmail.com>
2021-04-21 11:36:13 +0200

    Add entity class for skeleton horses

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/SkeletonHorseEntity.java
@@ -0,0 +1,10 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.AbstractHorseEntity;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftAnimals;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftHanging;
+import org.bukkit.entity.SkeletonHorse;
+
+public interface SkeletonHorseEntity extends AbstractHorseEntity<SkeletonHorseEntity> {
+
+}
