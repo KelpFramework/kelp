6a653267046f73bf35ef603a03917e346b13df26
pxav <pxav.studios@gmail.com>
2021-04-21 11:35:00 +0200

    Add entity class for thrown ender pearls

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/ThrownEnderPearlEntity.java
@@ -0,0 +1,8 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.KelpProjectile;
+import org.bukkit.entity.EnderPearl;
+
+public interface ThrownEnderPearlEntity extends KelpProjectile<ThrownEnderPearlEntity> {
+
+}
