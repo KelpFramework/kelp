7d8935ebbbf80d081da626e93b2b5fcf6af8cf3b
pxav <pxav.studios@gmail.com>
2021-04-21 11:38:12 +0200

    Add entity class for illusioners

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/IllusionerEntity.java
@@ -0,0 +1,6 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.IllagerEntity;
+
+public interface IllusionerEntity extends IllagerEntity<IllusionerEntity> {
+}
