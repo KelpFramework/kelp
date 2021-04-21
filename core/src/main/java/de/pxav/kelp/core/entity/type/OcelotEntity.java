85170b56e27ba0afe53d7175856f2c2193e616a7
pxav <pxav.studios@gmail.com>
2021-04-21 11:41:44 +0200

    Add entity class for ocelots

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/OcelotEntity.java
@@ -0,0 +1,8 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.AnimalEntity;
+import de.pxav.kelp.core.entity.type.general.TameableAnimal;
+
+public interface OcelotEntity extends AnimalEntity<OcelotEntity>, TameableAnimal<OcelotEntity> {
+
+}
