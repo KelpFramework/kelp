cdfcfacd95bccb6f964e5e195a9753c2771dd1be
pxav <pxav.studios@gmail.com>
2021-04-21 11:41:58 +0200

    Add entity class for llama spits

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/LlamaSpitEntity.java
@@ -0,0 +1,8 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.KelpProjectile;
+
+public interface LlamaSpitEntity extends KelpProjectile<LlamaSpitEntity> {
+
+
+}
