25aa05fe7673b6fd07c47e9c9764ddfe17eff763
pxav <pxav.studios@gmail.com>
2021-04-21 11:40:57 +0200

    Add entity class for mushroom cows

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/MushroomCowEntity.java
@@ -0,0 +1,11 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.util.MushroomCowType;
+
+public interface MushroomCowEntity extends CowEntity {
+
+  MushroomCowType getMushroomType();
+
+  MushroomCowEntity setMushroomType(MushroomCowType mushroomType);
+
+}
