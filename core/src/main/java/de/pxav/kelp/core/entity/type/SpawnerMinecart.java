18e9e7ba0a1962521c1ec1b35fa3f07d834f56ee
pxav <pxav.studios@gmail.com>
2021-04-21 11:39:41 +0200

    Add entity class for spawner minecart

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/SpawnerMinecart.java
@@ -0,0 +1,6 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.type.general.MinecartEntity;
+
+public interface SpawnerMinecart extends MinecartEntity<SpawnerMinecart> {
+}
