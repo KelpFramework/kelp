459ad783ed281c038d5d267c3a264ce01d9a8d21
pxav <pxav.studios@gmail.com>
2021-04-21 11:39:02 +0200

    Add entity class for lightnings

---
+++ core/src/main/java/de/pxav/kelp/core/entity/type/LightningEntity.java
@@ -0,0 +1,9 @@
+package de.pxav.kelp.core.entity.type;
+
+import de.pxav.kelp.core.entity.KelpEntity;
+
+public interface LightningEntity extends KelpEntity<LightningEntity> {
+
+  boolean isEffect();
+
+}
