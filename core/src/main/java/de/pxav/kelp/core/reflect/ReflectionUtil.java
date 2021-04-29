4c88d90384cea35a23b79232692280b4225bfa28
pxav <pxav.studios@gmail.com>
2021-04-29 09:26:55 +0200

    Add methods to invoke private methods in ReflectionUtil

--- core/src/main/java/de/pxav/kelp/core/reflect/ReflectionUtil.java
+++ core/src/main/java/de/pxav/kelp/core/reflect/ReflectionUtil.java
@@ -1,6 +1,10 @@
 package de.pxav.kelp.core.reflect;
 
+import com.google.common.base.Preconditions;
+
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * A class description goes here.
@@ -26,6 +30,56 @@ public class ReflectionUtil {
       field.setAccessible(true);
       field.set(object, value);
     } catch (NoSuchFieldException | IllegalAccessException e) {
+      e.printStackTrace();
+    }
+  }
+
+  // method name without "(" and ")"
+  public static void invokeMethod(Object holdingObject, String methodName, Object... parameters) {
+    Preconditions.checkNotNull(holdingObject);
+    Preconditions.checkNotNull(methodName);
+    try {
+
+      Method method;
+
+      if (parameters.length > 0) {
+        Class<?>[] parameterTypes = new Class[parameters.length];
+        for (int i = 0; i < parameters.length; i++) {
+          parameterTypes[i] = parameters[i].getClass();
+        }
+        method = holdingObject.getClass().getDeclaredMethod(methodName, parameterTypes);
+      } else {
+        method = holdingObject.getClass().getDeclaredMethod(methodName);
+      }
+
+      method.setAccessible(true);
+      method.invoke(holdingObject);
+    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+      e.printStackTrace();
+    }
+  }
+
+  // method name without "(" and ")"
+  public static void invokeStaticMethod(Class<?> holdingClass, String methodName, Object... parameters) {
+    Preconditions.checkNotNull(holdingClass);
+    Preconditions.checkNotNull(methodName);
+    try {
+
+      Method method;
+
+      if (parameters.length > 0) {
+        Class<?>[] parameterTypes = new Class[parameters.length];
+        for (int i = 0; i < parameters.length; i++) {
+          parameterTypes[i] = parameters[i].getClass();
+        }
+        method = holdingClass.getDeclaredMethod(methodName, parameterTypes);
+      } else {
+        method = holdingClass.getDeclaredMethod(methodName);
+      }
+
+      method.setAccessible(true);
+      method.invoke(null);
+    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
       e.printStackTrace();
     }
   }
