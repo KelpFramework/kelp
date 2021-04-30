4ced85f395c586fd5213a78d71545b8e99eebc48
pxav <pxav.studios@gmail.com>
2021-04-30 21:36:59 +0200

    Make FireworkMetadata a subtype of ItemMetadata

--- core/src/main/java/de/pxav/kelp/core/inventory/metadata/FireworkMetadata.java
+++ core/src/main/java/de/pxav/kelp/core/inventory/metadata/FireworkMetadata.java
@@ -6,7 +6,7 @@ import org.bukkit.FireworkEffect;
 import java.util.Arrays;
 import java.util.Collection;
 
-public class FireworkMetadata {
+public class FireworkMetadata implements ItemMetadata {
 
   private int height = 20;
   private Collection<FireworkEffect> effects = Lists.newArrayList();
