ffc7d705d0051f4d85806b27687dc192aa8a7ef6
DSeeLP <46624152+DSeeLP@users.noreply.github.com>
2021-05-02 11:05:30 +0200

    Bumped KommonCommand version to 0.1.1
    Signed-off-by: DSeeLP <46624152+DSeeLP@users.noreply.github.com>

--- core/src/main/java/de/dseelp/kommon/command/arguments/UUIDArgument.kt
+++ core/src/main/java/de/dseelp/kommon/command/arguments/UUIDArgument.kt
@@ -6,16 +6,18 @@ import java.util.*
 /**
  * @author DSeeLP
  */
-class UUIDArgument<S: Any>(name: String, val completer: CommandContext<S>.() -> Array<UUID> = { arrayOf() }) : Argument<S, UUID>(name) {
-    constructor(name: String): this(name, { arrayOf() })
-
+class UUIDArgument<S : Any> @JvmOverloads constructor(
+    name: String,
+    val completer: CommandContext<S>.() -> Array<UUID> = { arrayOf() }
+) : Argument<S, UUID>(name) {
     override fun get(value: String): UUID? = value.toUUIDOrNull()
     override fun getErrorMessage(): String = "%s is not a UUID"
-    override fun complete(context: CommandContext<S>, value: String): Array<String> = completer.invoke(context).map { it.toString() }.filter { value.startsWith(it, true) }.toTypedArray()
+    override fun complete(context: CommandContext<S>, value: String): Array<String> =
+        completer.invoke(context).map { it.toString() }.filter { value.startsWith(it, true) }.toTypedArray()
 }
 
 fun String.toUUIDOrNull(): UUID? = try {
     UUID.fromString(this)
-}catch (ex: IllegalArgumentException) {
+} catch (ex: IllegalArgumentException) {
     null
 }
