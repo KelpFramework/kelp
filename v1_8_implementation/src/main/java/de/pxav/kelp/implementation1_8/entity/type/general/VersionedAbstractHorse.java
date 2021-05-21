3aaf194904345b09c9c85ae1e9eb0f8156769298
pxav <pxav.studios@gmail.com>
2021-05-21 12:22:47 +0200

    Fix class cast exception when spawning a horse

--- v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/entity/type/general/VersionedAbstractHorse.java
+++ v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/entity/type/general/VersionedAbstractHorse.java
@@ -1,5 +1,6 @@
 package de.pxav.kelp.implementation1_8.entity.type.general;
 
+import de.pxav.kelp.core.entity.KelpEntity;
 import de.pxav.kelp.core.entity.KelpEntityType;
 import de.pxav.kelp.core.entity.type.general.AbstractHorseEntity;
 import de.pxav.kelp.core.entity.version.EntityTypeVersionTemplate;
@@ -8,11 +9,16 @@ import de.pxav.kelp.core.inventory.type.HorseInventory;
 import de.pxav.kelp.core.inventory.version.InventoryVersionTemplate;
 import net.minecraft.server.v1_8_R3.Entity;
 import net.minecraft.server.v1_8_R3.EntityHorse;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftHorse;
+import org.bukkit.entity.Horse;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
 
 public class VersionedAbstractHorse<T extends AbstractHorseEntity<T>>
-  extends VersionedTameableAnimal<T>
+  extends VersionedAnimalEntity<T>
   implements AbstractHorseEntity<T> {
 
   private EntityHorse horseHandle;
@@ -66,6 +72,37 @@ public class VersionedAbstractHorse<T extends AbstractHorseEntity<T>>
   @Override
   public T setMaximumDomestication(int maximumDomestication) {
     horseHandle.maxDomestication = maximumDomestication;
+    return (T) this;
+  }
+
+  @Override
+  public T setTamed(boolean tamed) {
+    horseHandle.setTame(tamed);
+    return (T) this;
+  }
+
+  @Override
+  public boolean isTamed() {
+    return horseHandle.isTame();
+  }
+
+  @Override
+  public KelpEntity<?> getOwner() {
+    Player player = Bukkit.getPlayer(getOwnerUUID());
+    if (player == null) {
+      return null;
+    }
+    return KelpEntity.from(player);
+  }
+
+  @Override
+  public UUID getOwnerUUID() {
+    return UUID.fromString(horseHandle.getOwnerUUID());
+  }
+
+  @Override
+  public T setOwner(KelpEntity<?> owner) {
+    horseHandle.setOwnerUUID(owner.getUUID().toString());
     return (T) this;
   }
 
