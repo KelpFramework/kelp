494fd05ecb6bde0abaefca7cdfb9f90835c6dea8
pxav <pxav.studios@gmail.com>
2021-05-21 16:40:57 +0200

    Add documentation for absorption potion effect

--- core/src/main/java/de/pxav/kelp/core/entity/util/potion/minecraft/AbsorptionPotionEffect.java
+++ core/src/main/java/de/pxav/kelp/core/entity/util/potion/minecraft/AbsorptionPotionEffect.java
@@ -2,12 +2,24 @@ package de.pxav.kelp.core.entity.util.potion.minecraft;
 
 import de.pxav.kelp.core.entity.util.potion.KelpPotionEffect;
 import de.pxav.kelp.core.inventory.metadata.Color;
+import de.pxav.kelp.core.version.KelpVersion;
+import org.bukkit.potion.PotionEffectType;
 
+/**
+ * Represents the absorption potion effect. Absorption is a status
+ * effect that pads the health bar with extra hearts.
+ *
+ * Absorption adds 4 additional health points per level to the player,
+ * displayed as yellow hearts above the normal health bar. If the player takes damage while under this effect,
+ * the absorption health points are depleted before the normal health points.
+ *
+ * @author pxav
+ */
 public class AbsorptionPotionEffect extends KelpPotionEffect {
 
   @Override
   public String getName() {
-    return null;
+    return "Absorption";
   }
 
   @Override
@@ -17,12 +29,34 @@ public class AbsorptionPotionEffect extends KelpPotionEffect {
 
   @Override
   public Color getColor() {
-    return null;
+    return Color.fromHEX("2552A5");
   }
 
   @Override
-  public int getDurationForLevel(int level) {
-    return 0;
+  public boolean isBukkitEffectUnsafe(KelpVersion version) {
+    return true;
+  }
+
+  /**
+   * Gets the amount of extra health points (HP) for the given level.
+   * Two health points are equal to 1 heart.
+   *
+   * @param level The absorption level to get the HP for.
+   * @return The HP given by this level
+   */
+  public int getHealthPointsForLevel(int level) {
+    return level * 4;
+  }
+
+  /**
+   * Gets the amount of extra hearts a player receives when the given
+   * level of absorption is applied.
+   *
+   * @param level The level to get the amount of hearts for.
+   * @return The hearts given by this level.
+   */
+  public int getHeartsForLevel(int level) {
+    return level * 2;
   }
 
 }
