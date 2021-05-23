c0d485e6675f274ff7cdfbff8d004664d43e819c
pxav <pxav.studios@gmail.com>
2021-05-23 12:10:29 +0200

    Add methods to identify entity groups such as undead or bosses

--- core/src/main/java/de/pxav/kelp/core/entity/KelpEntityType.java
+++ core/src/main/java/de/pxav/kelp/core/entity/KelpEntityType.java
@@ -4,9 +4,11 @@ import de.pxav.kelp.core.entity.type.*;
 import de.pxav.kelp.core.entity.type.general.MinecartEntity;
 import de.pxav.kelp.core.player.KelpPlayer;
 import de.pxav.kelp.core.version.KelpVersion;
+import org.bukkit.entity.EntityType;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Set;
 
 /**
  * A class description goes here.
@@ -18,9 +20,18 @@ public enum  KelpEntityType {
   DROPPED_ITEM(KelpVersion.MC_1_8_0, DroppedItemEntity.class),
   EXPERIENCE_ORB(KelpVersion.MC_1_8_0, ExperienceOrbEntity.class),
   AREA_EFFECT_CLOUD(KelpVersion.MC_1_9_0, AreaEffectCloudEntity.class),
-  ELDER_GUARDIAN(KelpVersion.MC_1_8_0, ElderGuardianEntity.class),
-  WITHER_SKELETON(KelpVersion.MC_1_8_0, WitherSkeletonEntity.class),
-  STRAY_SKELETON(KelpVersion.MC_1_10_0, StraySkeletonEntity.class),
+  ELDER_GUARDIAN(KelpVersion.MC_1_8_0, ElderGuardianEntity.class) {
+    public boolean isHostile() { return true; }
+    public boolean isAquatic() { return true; }
+  },
+  WITHER_SKELETON(KelpVersion.MC_1_8_0, WitherSkeletonEntity.class) {
+    public boolean isUndead() { return true; }
+    public boolean isHostile() { return true; }
+  },
+  STRAY_SKELETON(KelpVersion.MC_1_10_0, StraySkeletonEntity.class) {
+    public boolean isUndead() { return true; }
+    public boolean isHostile() { return true; }
+  },
   CHICKEN_EGG(KelpVersion.MC_1_8_0, ThrownChickenEggEntity.class),
   LEASH_HITCH(KelpVersion.MC_1_8_0, LeashHitchEntity.class),
   PAINTING(KelpVersion.MC_1_8_0, PaintingEntity.class),
@@ -37,20 +48,36 @@ public enum  KelpEntityType {
   PRIMED_TNT(KelpVersion.MC_1_8_0, PrimedTntEntity.class),
   FALLING_BLOCK(KelpVersion.MC_1_8_0, FallingBlockEntity.class),
   FIREWORK(KelpVersion.MC_1_8_0, FireworkEntity.class),
-  HUSK(KelpVersion.MC_1_10_0, HuskEntity.class),
+  HUSK(KelpVersion.MC_1_10_0, HuskEntity.class) {
+    public boolean isUndead() { return true; }
+    public boolean isHostile() { return true; }
+  },
   SPECTRAL_ARROW(KelpVersion.MC_1_9_0, SpectralArrowEntity.class),
   SHULKER_BULLET(KelpVersion.MC_1_9_0, ShulkerBulletEntity.class),
   DRAGON_FIREBALL(KelpVersion.MC_1_9_0, DragonFireballEntity.class),
-  ZOMBIE_VILLAGER(KelpVersion.MC_1_8_0, ZombieVillagerEntity.class),
-  SKELETON_HORSE(KelpVersion.MC_1_8_0, SkeletonHorseEntity.class),
-  ZOMBIE_HORSE(KelpVersion.MC_1_8_0, ZombieHorse.class),
+  ZOMBIE_VILLAGER(KelpVersion.MC_1_8_0, ZombieVillagerEntity.class) {
+    public boolean isUndead() { return true; }
+    public boolean isHostile() { return true; }
+  },
+  SKELETON_HORSE(KelpVersion.MC_1_8_0, SkeletonHorseEntity.class) {
+    public boolean isUndead() { return true; }
+  },
+  ZOMBIE_HORSE(KelpVersion.MC_1_8_0, ZombieHorse.class) {
+    public boolean isUndead() { return true; }
+  },
   ARMOR_STAND(KelpVersion.MC_1_8_0, ArmorStandEntity.class),
   DONKEY(KelpVersion.MC_1_8_0, DonkeyEntity.class),
   MULE(KelpVersion.MC_1_8_0, MuleEntity.class),
   EVOKER_FANGS(KelpVersion.MC_1_11_0, EvokerFangEntity.class),
-  EVOKER(KelpVersion.MC_1_11_0, EvokerEntity.class),
-  VEX(KelpVersion.MC_1_11_0, VexEntity.class),
-  VINDICATOR(KelpVersion.MC_1_11_0, VindicatorEntity.class),
+  EVOKER(KelpVersion.MC_1_11_0, EvokerEntity.class) {
+    public boolean isHostile() { return true; }
+  },
+  VEX(KelpVersion.MC_1_11_0, VexEntity.class) {
+    public boolean isHostile() { return true; }
+  },
+  VINDICATOR(KelpVersion.MC_1_11_0, VindicatorEntity.class) {
+    public boolean isHostile() { return true; }
+  },
   ILLUSIONER(KelpVersion.MC_1_12_0, IllusionerEntity.class),
   MINECART_COMMAND(KelpVersion.MC_1_8_0, CommandMinecartEntity.class),
   BOAT(KelpVersion.MC_1_8_0, BoatEntity.class),
@@ -60,31 +87,69 @@ public enum  KelpEntityType {
   MINECART_TNT(KelpVersion.MC_1_8_0, ExplosiveMinecart.class),
   MINECART_HOPPER(KelpVersion.MC_1_8_0, HopperMinecart.class),
   MINECART_MOB_SPAWNER(KelpVersion.MC_1_8_0, SpawnerMinecart.class),
-  CREEPER(KelpVersion.MC_1_8_0, CreeperEntity.class),
-  SKELETON(KelpVersion.MC_1_8_0, SkeletonEntity.class),
+  CREEPER(KelpVersion.MC_1_8_0, CreeperEntity.class) {
+    public boolean isHostile() { return true; }
+  },
+  SKELETON(KelpVersion.MC_1_8_0, SkeletonEntity.class) {
+    public boolean isUndead() { return true; }
+    public boolean isHostile() { return true; }
+  },
   SPIDER(KelpVersion.MC_1_8_0, SpiderEntity.class),
-  ZOMBIE_GIANT(KelpVersion.MC_1_8_0, ZombieGiant.class),
-  ZOMBIE(KelpVersion.MC_1_8_0, ZombieEntity.class),
-  SLIME(KelpVersion.MC_1_8_0, SlimeEntity.class),
-  GHAST(KelpVersion.MC_1_8_0, GhastEntity.class),
-  PIG_ZOMBIE(KelpVersion.MC_1_8_0, PigZombie.class),
+  ZOMBIE_GIANT(KelpVersion.MC_1_8_0, ZombieGiant.class) {
+    public boolean isUndead() { return true; }
+  },
+  ZOMBIE(KelpVersion.MC_1_8_0, ZombieEntity.class) {
+    public boolean isUndead() { return true; }
+    public boolean isHostile() { return true; }
+  },
+  SLIME(KelpVersion.MC_1_8_0, SlimeEntity.class) {
+    public boolean isHostile() { return true; }
+  },
+  GHAST(KelpVersion.MC_1_8_0, GhastEntity.class) {
+    public boolean isHostile() { return true; }
+  },
+  PIG_ZOMBIE(KelpVersion.MC_1_8_0, PigZombie.class) {
+    public boolean isUndead() { return true; }
+  },
   ENDERMAN(KelpVersion.MC_1_8_0, EndermanEntity.class),
   CAVE_SPIDER(KelpVersion.MC_1_8_0, CaveSpiderEntity.class),
-  SILVERFISH(KelpVersion.MC_1_8_0, SilverfishEntity.class),
-  BLAZE(KelpVersion.MC_1_8_0, BlazeEntity.class),
-  MAGMA_CUBE(KelpVersion.MC_1_8_0, MagmaCubeEntity.class),
-  ENDER_DRAGON(KelpVersion.MC_1_8_0, EnderDragonEntity.class),
-  WITHER(KelpVersion.MC_1_8_0, WitherEntity.class),
+  SILVERFISH(KelpVersion.MC_1_8_0, SilverfishEntity.class) {
+    public boolean isHostile() { return true; }
+  },
+  BLAZE(KelpVersion.MC_1_8_0, BlazeEntity.class) {
+    public boolean isHostile() { return true; }
+  },
+  MAGMA_CUBE(KelpVersion.MC_1_8_0, MagmaCubeEntity.class) {
+    public boolean isHostile() { return true; }
+  },
+  ENDER_DRAGON(KelpVersion.MC_1_8_0, EnderDragonEntity.class) {
+    public boolean isBoss() { return true; }
+  },
+  WITHER(KelpVersion.MC_1_8_0, WitherEntity.class) {
+    public boolean isUndead() { return true; }
+    public boolean isBoss() { return true; }
+  },
   BAT(KelpVersion.MC_1_8_0, BatEntity.class),
-  WITCH(KelpVersion.MC_1_8_0, WitchEntity.class),
-  ENDERMITE(KelpVersion.MC_1_8_0, EndermiteEntity.class),
-  GUARDIAN(KelpVersion.MC_1_8_0, GuardianEntity.class),
-  SHULKER(KelpVersion.MC_1_9_0, ShulkerEntity.class),
+  WITCH(KelpVersion.MC_1_8_0, WitchEntity.class) {
+    public boolean isHostile() { return true; }
+  },
+  ENDERMITE(KelpVersion.MC_1_8_0, EndermiteEntity.class) {
+    public boolean isHostile() { return true; }
+  },
+  GUARDIAN(KelpVersion.MC_1_8_0, GuardianEntity.class) {
+    public boolean isHostile() { return true; }
+    public boolean isAquatic() { return true; }
+  },
+  SHULKER(KelpVersion.MC_1_9_0, ShulkerEntity.class) {
+    public boolean isHostile() { return true; }
+  },
   PIG(KelpVersion.MC_1_8_0, PigEntity.class),
   SHEEP(KelpVersion.MC_1_8_0, SheepEntity.class),
   COW(KelpVersion.MC_1_8_0, CowEntity.class),
   CHICKEN(KelpVersion.MC_1_8_0, ChickenEntity.class),
-  SQUID(KelpVersion.MC_1_8_0, SquidEntity.class),
+  SQUID(KelpVersion.MC_1_8_0, SquidEntity.class) {
+    public boolean isAquatic() { return true; }
+  },
   WOLF(KelpVersion.MC_1_8_0, WolfEntity.class),
   MUSHROOM_COW(KelpVersion.MC_1_8_0, MushroomCowEntity.class),
   SNOWMAN(KelpVersion.MC_1_8_0, SnowmanEntity.class),
@@ -98,19 +163,40 @@ public enum  KelpEntityType {
   PARROT(KelpVersion.MC_1_12_0, ParrotEntity.class),
   VILLAGER(KelpVersion.MC_1_8_0, VillagerEntity.class),
   ENDER_CRYSTAL(KelpVersion.MC_1_8_0, EnderCrystalEntity.class),
-  TURTLE(KelpVersion.MC_1_13_0, TurtleEntity.class),
-  PHANTOM(KelpVersion.MC_1_13_0, PhantomEntity.class),
+  TURTLE(KelpVersion.MC_1_13_0, TurtleEntity.class) {
+    public boolean isAquatic() { return true; }
+  },
+  PHANTOM(KelpVersion.MC_1_13_0, PhantomEntity.class) {
+    public boolean isHostile() { return true; }
+  },
   TRIDENT(KelpVersion.MC_1_13_0, ThrownTridentEntity.class),
-  COD(KelpVersion.MC_1_13_0, CodEntity.class),
-  SALMON(KelpVersion.MC_1_13_0, SalmonEntity.class),
-  PUFFERFISH(KelpVersion.MC_1_13_0, PufferfishEntity.class),
-  TROPICAL_FISH(KelpVersion.MC_1_13_0, TropicalFishEntity.class),
-  DROWNED(KelpVersion.MC_1_13_0, DrownedEntity.class),
-  DOLPHIN(KelpVersion.MC_1_13_0, DolphinEntity.class),
+  COD(KelpVersion.MC_1_13_0, CodEntity.class) {
+    public boolean isAquatic() { return true; }
+  },
+  SALMON(KelpVersion.MC_1_13_0, SalmonEntity.class) {
+    public boolean isAquatic() { return true; }
+  },
+  PUFFERFISH(KelpVersion.MC_1_13_0, PufferfishEntity.class) {
+    public boolean isAquatic() { return true; }
+  },
+  TROPICAL_FISH(KelpVersion.MC_1_13_0, TropicalFishEntity.class) {
+    public boolean isAquatic() { return true; }
+  },
+  DROWNED(KelpVersion.MC_1_13_0, DrownedEntity.class) {
+    public boolean isUndead() { return true; }
+    public boolean isHostile() { return true; }
+  },
+  DOLPHIN(KelpVersion.MC_1_13_0, DolphinEntity.class) {
+    public boolean isAquatic() { return true; }
+  },
   CAT(KelpVersion.MC_1_8_0, CatEntity.class),
   PANDA(KelpVersion.MC_1_14_0, PandaEntity.class),
-  PILLAGER(KelpVersion.MC_1_14_0, PillagerEntity.class),
-  RAVAGER(KelpVersion.MC_1_14_0, RavagerEntity.class),
+  PILLAGER(KelpVersion.MC_1_14_0, PillagerEntity.class) {
+    public boolean isHostile() { return true; }
+  },
+  RAVAGER(KelpVersion.MC_1_14_0, RavagerEntity.class) {
+    public boolean isHostile() { return true; }
+  },
   TRADER_LLAMA(KelpVersion.MC_1_14_0, TraderLlamaEntity.class),
   WANDERING_TRADER(KelpVersion.MC_1_14_0, WanderingTraderEntity.class),
   FOX(KelpVersion.MC_1_14_0, FoxEntity.class),
@@ -120,7 +206,6 @@ public enum  KelpEntityType {
   UNKNOWN(KelpVersion.MC_1_8_0, null);
 
   private KelpVersion since;
-  //private Class<? extends KelpEntity> entityClass;
   private Class<? extends KelpEntity<?>> entityClass;
 
   KelpEntityType(KelpVersion since, Class<? extends KelpEntity<?>> entityClass) {
@@ -132,16 +217,33 @@ public enum  KelpEntityType {
     return since;
   }
 
-//  public Class<? extends KelpEntity> getEntityClass() {
-//    return entityClass;
-//  }
-
   public Class<?> getEntityClass() {
     return entityClass;
   }
 
   public static boolean isLivingEntity(KelpEntityType entityType) {
     return entityType == KelpEntityType.ZOMBIE;
+  }
+
+  public boolean isUndead() {
+    return false;
+  }
+
+  public boolean isAquatic() {
+    return false;
+  }
+
+  // TODO IMPLEMENT!!!!
+  public boolean isArthropod() {
+    return false;
+  }
+
+  public boolean isBoss() {
+    return false;
+  }
+
+  public boolean isHostile() {
+    return false;
   }
 
   public static Collection<KelpEntityType> aboveVersion(KelpVersion version) {
