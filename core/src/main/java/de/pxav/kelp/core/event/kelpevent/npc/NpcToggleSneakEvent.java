4d05af00b1dcc8c03f298c46f9049cc5e464c9a4
pxav <pxav.studios@gmail.com>
2021-05-25 10:06:15 +0200

    Remove redundant field from NpcToggleSneakEvent

--- core/src/main/java/de/pxav/kelp/core/event/kelpevent/npc/NpcToggleSneakEvent.java
+++ core/src/main/java/de/pxav/kelp/core/event/kelpevent/npc/NpcToggleSneakEvent.java
@@ -11,7 +11,6 @@ import org.bukkit.event.HandlerList;
 public class NpcToggleSneakEvent extends NpcEvent {
 
   private static final HandlerList handlers = new HandlerList();
-  private boolean initialSpawn;
 
   public NpcToggleSneakEvent(KelpNpc npc) {
     super(npc);
