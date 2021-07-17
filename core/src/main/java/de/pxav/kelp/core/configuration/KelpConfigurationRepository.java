e5dd18b8409367fb7682660c740958bbe1d03433
PXAV <pxav.studios@gmail.com>
2021-07-17 16:05:00 +0200

    Add config repository class allowing to register/load config class from a plugin

---
+++ core/src/main/java/de/pxav/kelp/core/configuration/KelpConfigurationRepository.java
@@ -0,0 +1,38 @@
+package de.pxav.kelp.core.configuration;
+
+import com.google.inject.Injector;
+import de.pxav.kelp.core.configuration.validate.ConfigValidationException;
+import de.pxav.kelp.core.configuration.validate.ValidatableConfig;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class KelpConfigurationRepository {
+
+  private JavaPlugin kelpPlugin;
+  private Injector injector;
+
+  @Inject
+  public KelpConfigurationRepository(JavaPlugin kelpPlugin, Injector injector) {
+    this.kelpPlugin = kelpPlugin;
+    this.injector = injector;
+  }
+
+  public void registerConfig(Class<? extends KelpConfiguration> configurationClass) {
+    KelpConfiguration configInstance = injector.getInstance(configurationClass);
+
+    configInstance.load();
+
+    if (configInstance instanceof ValidatableConfig) {
+      ValidatableConfig validatableConfig = (ValidatableConfig) configInstance;
+      try {
+        validatableConfig.validate();
+      } catch (ConfigValidationException e) {
+        e.logValidationError();
+      }
+    }
+  }
+
+}
