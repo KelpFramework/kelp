862ff72ad6f5fd260822eebb11d75a0c16788864
PXAV <pxav.studios@gmail.com>
2021-07-17 16:04:10 +0200

    Add class representing the kelp default configuration - this serves as an example of how to use the config library - it also replaces the old default config and handles settings such as debug mode, etc.

--- core/src/main/java/de/pxav/kelp/core/configuration/internal/KelpDefaultConfiguration.java
+++ core/src/main/java/de/pxav/kelp/core/configuration/internal/KelpDefaultConfiguration.java
@@ -1,30 +1,50 @@
 package de.pxav.kelp.core.configuration.internal;
 
-import com.google.inject.Singleton;
-import de.pxav.kelp.core.configuration.Configuration;
-import de.pxav.kelp.core.configuration.ConfigurationAttribute;
-import de.pxav.kelp.core.configuration.KelpConfiguration;
-import de.pxav.kelp.core.configuration.type.PropertiesConfigurationType;
+import de.pxav.kelp.core.configuration.validate.IntegerValidator;
+import de.pxav.kelp.core.configuration.validate.ValidatableConfig;
 
-/**
- * A class description goes here.
- *
- * @author pxav
- */
+import javax.inject.Singleton;
+import java.io.File;
+
 @Singleton
-@Configuration(name = "config",
-        path = "plugins//Kelp",
-        type = PropertiesConfigurationType.class
-)
-public final class KelpDefaultConfiguration extends KelpConfiguration {
+public class KelpDefaultConfiguration extends ValidatableConfig {
 
+  /**
+   * @return Final destination of the config file
+   */
   @Override
-  public void defineDefaults() {
-    add(developmentMode(), false);
+  protected File configFile() {
+    return new File("plugins//Kelp", "kelp-config.yml");
   }
 
-  public String developmentMode() {
-    return "development-mode";
+  /**
+   * @return The location of the config blueprint file
+   *         (including documentation and default values)
+   */
+  @Override
+  protected String resourceTemplatePath() {
+    return "core/kelp-config.yml";
+  }
+
+  /**
+   * Register all validators that have to pass before the
+   * config can be properly used. Checked on every enable.
+   */
+  @Override
+  public void setValidators() {
+
+    // tick rate must be positive and low enough
+    // to avoid stuttering animations
+    addValidator(KelpConfigValue.NPC_TICK_RATE,
+      IntegerValidator.nonNegative(),
+      IntegerValidator.failIfGreaterThan(1000));
+
+    // there are only 3 options, so fail if any index
+    // out of that range is chosen
+    addValidator(KelpConfigValue.CONFIG_PATCH_MODE,
+      IntegerValidator.nonNegative(),
+      IntegerValidator.failIfGreaterThan(2));
+
   }
 
 }
