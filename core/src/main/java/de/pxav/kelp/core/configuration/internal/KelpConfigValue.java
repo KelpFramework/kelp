b2b34bb61a80ae7cd3a95351b158e7c680423adf
PXAV <pxav.studios@gmail.com>
2021-07-17 16:00:13 +0200

    Add class containing constants with the names of the keys contained by the default config

---
+++ core/src/main/java/de/pxav/kelp/core/configuration/internal/KelpConfigValue.java
@@ -0,0 +1,14 @@
+package de.pxav.kelp.core.configuration.internal;
+
+public class KelpConfigValue {
+
+  public static final String DEBUG_MODE = "general.debug-mode";
+
+  public static final String AUTO_UPDATE_CONFIGS = "config.auto-update";
+  public static final String BACKUP_CONFIG_BEFORE_UPDATE = "config.backup-before-update";
+
+  public static final String CONFIG_PATCH_MODE = "config.backup-before-update";
+
+  public static final String NPC_TICK_RATE = "npc.tick-rate";
+
+}
