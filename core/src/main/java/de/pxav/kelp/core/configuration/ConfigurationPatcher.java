efd02431b71f35db6fd4c93d029d3882d0d9f5a7
PXAV <pxav.studios@gmail.com>
2021-07-17 16:03:19 +0200

    Add patcher class ensuring that a patch of a config is necessary and executes the dumper and parser accordingly

---
+++ core/src/main/java/de/pxav/kelp/core/configuration/ConfigurationPatcher.java
@@ -0,0 +1,59 @@
+package de.pxav.kelp.core.configuration;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import de.pxav.kelp.core.common.KelpFileUtils;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class ConfigurationPatcher {
+
+  private final YamlConfiguration templateConfig;
+  private final YamlConfiguration existingConfig;
+
+  private Map<String, Object> valueBackup;
+
+  public static ConfigurationPatcher create(YamlConfiguration templateFile, YamlConfiguration patchedFile) {
+    return new ConfigurationPatcher(templateFile, patchedFile);
+  }
+
+  private ConfigurationPatcher(YamlConfiguration templateConfig, YamlConfiguration existingConfig) {
+    this.templateConfig = templateConfig;
+    this.existingConfig = existingConfig;
+    this.valueBackup = Maps.newHashMap();
+  }
+
+  public boolean patchNecessary() {
+    Set<String> templateKeys = templateConfig.getKeys(true);
+
+    if (existingConfig.getKeys(true).containsAll(templateKeys)) {
+      System.out.println("Files are equal. No patching required!");
+      return false;
+    }
+
+    return true;
+  }
+
+  public void backupValues() {
+    this.valueBackup = existingConfig.getValues(true);
+  }
+
+  public void patch(String resourcePath, File outputFile) {
+    KelpFileUtils.saveResource(resourcePath, outputFile);
+
+    List<String> patchedContents = ConfigurationParser
+      .create(outputFile)
+      .parseContents(this.valueBackup);
+
+    ConfigurationDumper.create(outputFile).dump(patchedContents);
+
+  }
+
+}
