8dfc82105aecd7c1109e38abbcc5765048451023
pxav <pxav.studios@gmail.com>
2021-08-15 21:30:59 +0200

    Add base class for AnimatedMessages - this system is planned to release in the future, but not with the next release

---
+++ core/src/main/java/de/pxav/kelp/core/player/message/image/AnimatedMessage.java
@@ -0,0 +1,15 @@
+package de.pxav.kelp.core.player.message.image;
+
+/**
+ *
+ * Credits for this library go to __, who published some useful classes on
+ * <a href="https://github.com/arxenix/ImageMessage">GitHub</a>. I customized
+ * and optimized lots of methods for better integration into Kelp, but the
+ * base for this class comes from him.
+ *
+ * @author pxav
+ * @author arxenix
+ */
+public class AnimatedMessage {
+
+}
