d01c53cd655d235cadb46255b7e4f3a583e8cbb7
pxav <pxav.studios@gmail.com>
2021-08-15 15:36:00 +0200

    Implement ParticleVisualizable for Rays

--- core/src/main/java/de/pxav/kelp/core/world/raycast/Ray.java
+++ core/src/main/java/de/pxav/kelp/core/world/raycast/Ray.java
@@ -1,18 +1,25 @@
 package de.pxav.kelp.core.world.raycast;
 
+import de.pxav.kelp.core.particle.visualize.ParticleVisualizable;
+import de.pxav.kelp.core.particle.visualize.ParticleVisualizerProfile;
+import de.pxav.kelp.core.player.KelpPlayer;
 import de.pxav.kelp.core.world.KelpLocation;
-import org.bukkit.util.Vector;
+import de.pxav.kelp.core.world.util.Vector3;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public abstract class Ray {
+public abstract class Ray implements ParticleVisualizable {
 
   protected KelpLocation startLocation;
-  protected Vector direction;
+  protected Vector3 direction;
 
   protected double maxLength = 20.0d;
   protected int maxCollisions = 1;
+
+  protected KelpPlayer[] visualizeTo;
+  protected boolean visualize;
+  protected ParticleVisualizerProfile visualizerProfile;
 
   public Ray maxLength(double maxLength) {
     this.maxLength = maxLength;
@@ -24,7 +31,7 @@ public abstract class Ray {
     return this;
   }
 
-  public Ray direction(Vector direction) {
+  public Ray direction(Vector3 direction) {
     this.direction = direction;
     return this;
   }
@@ -41,12 +48,19 @@ public abstract class Ray {
     return startLocation;
   }
 
-  public Vector getDirection() {
+  public Vector3 getDirection() {
     return direction;
   }
 
   public double getMaxLength() {
     return maxLength;
+  }
+
+  @Override
+  public void visualize(KelpPlayer player, ParticleVisualizerProfile visualizerProfile) {
+    this.visualize = true;
+    this.visualizeTo = new KelpPlayer[] {player};
+    this.visualizerProfile = visualizerProfile;
   }
 
 }
