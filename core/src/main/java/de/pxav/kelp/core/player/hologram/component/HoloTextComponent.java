2f615e280a1babeaca413a63f2141fe78d174c3a
pxav <pxav.studios@gmail.com>
2021-08-15 21:33:03 +0200

    Remove component system of hologram library

--- core/src/main/java/de/pxav/kelp/core/player/hologram/component/HoloTextComponent.java
+++
@@ -1,9 +0,0 @@
-package de.pxav.kelp.core.player.hologram.component;
-
-import java.util.Collection;
-
-public abstract class HoloTextComponent<T extends HoloTextComponent<T>> extends HologramComponent<T> {
-
-  public abstract Collection<String> getLines();
-
-}
