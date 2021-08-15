3238a4827dcf2c5bd373a40a82428a6c6de8536d
pxav <pxav.studios@gmail.com>
2021-08-15 21:42:46 +0200

    Implement new line-based hologram system for 1.8 servers

--- v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/player/VersionedHologram.java
+++ v1_8_implementation/src/main/java/de/pxav/kelp/implementation1_8/player/VersionedHologram.java
@@ -2,15 +2,12 @@ package de.pxav.kelp.implementation1_8.player;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import de.pxav.kelp.core.common.ConcurrentMultimap;
-import de.pxav.kelp.core.common.ConcurrentSetMultimap;
-import de.pxav.kelp.core.player.KelpPlayer;
-import de.pxav.kelp.core.player.hologram.HologramVersionTemplate;
-import de.pxav.kelp.core.player.hologram.KelpHologram;
-import de.pxav.kelp.core.player.hologram.component.EmptyTextComponent;
-import de.pxav.kelp.core.player.hologram.component.HoloItemComponent;
-import de.pxav.kelp.core.player.hologram.component.HoloTextComponent;
-import de.pxav.kelp.core.player.hologram.component.HologramComponent;
+import de.pxav.kelp.core.player.hologram.*;
+import de.pxav.kelp.core.player.hologram.line.EmptyHologramLine;
+import de.pxav.kelp.core.player.hologram.line.HologramLine;
+import de.pxav.kelp.core.player.hologram.line.ItemHologramLine;
+import de.pxav.kelp.core.player.hologram.line.TextHologramLine;
+import de.pxav.kelp.core.reflect.ReflectionUtil;
 import de.pxav.kelp.core.version.Versioned;
 import net.minecraft.server.v1_8_R3.*;
 import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
@@ -18,15 +15,17 @@ import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import javax.inject.Inject;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Versioned
 public class VersionedHologram extends HologramVersionTemplate {
 
   private static double TITLE_LINE_HEIGHT = 0.25;
-  private static ConcurrentMultimap<KelpHologram, Integer> entityIds = ConcurrentSetMultimap.create();
+
+  @Inject
+  private HologramRepository hologramRepository;
 
   @Override
   public void spawnHologram(KelpHologram hologram) {
@@ -35,10 +34,10 @@ public class VersionedHologram extends HologramVersionTemplate {
     CraftPlayer craftPlayer = (CraftPlayer) hologram.getPlayer().getBukkitPlayer();
     WorldServer worldServer = ((CraftWorld) (craftPlayer.getWorld())).getHandle();
 
-    for (HologramComponent<?> component : hologram.getComponents()) {
-      if (component instanceof HoloItemComponent) {
-        HoloItemComponent itemComponent = (HoloItemComponent) component;
-        ItemStack craftItemStack = itemComponent.getItem().getItemStack();
+    for (HologramLine<?> line : hologram.getLines()) {
+      if (line instanceof ItemHologramLine) {
+        ItemHologramLine itemComponent = (ItemHologramLine) line;
+        ItemStack craftItemStack = itemComponent.display().getItemStack();
         net.minecraft.server.v1_8_R3.ItemStack nmsItemStack = CraftItemStack.asNMSCopy(craftItemStack);
         EntityItem itemEntity = new EntityItem(worldServer);
         itemEntity.setCustomNameVisible(false);
@@ -50,30 +49,37 @@ public class VersionedHologram extends HologramVersionTemplate {
         NBTTagCompound tag = new NBTTagCompound();
         tag.setInt("DisabledSlots", 2039589); // All slots are disabled to prevent client glitches
         holoBase.a(tag);
-        
+
+        int armorStandId = randomEntityId();
+        int itemId = randomEntityId();
+        ReflectionUtil.setValue(Entity.class, holoBase, "id", armorStandId);
+        ReflectionUtil.setValue(Entity.class, itemEntity, "id", itemId);
+        hologram.addEntityId(armorStandId);
+        itemComponent.setItemEntityId(itemId);
+
         itemBases.put(itemEntity, holoBase);
         entities.add(itemEntity);
       }
 
-      if (component instanceof HoloTextComponent) {
-        HoloTextComponent<?> textComponent = (HoloTextComponent<?>) component;
+      if (line instanceof TextHologramLine) {
+        TextHologramLine textComponent = (TextHologramLine) line;
 
-        if (component instanceof EmptyTextComponent) {
-          entities.add(null);
-          continue;
-        }
+        EntityArmorStand holoBase = new EntityArmorStand(worldServer);
+        holoBase.setInvisible(true);
+        holoBase.setArms(false);
+        holoBase.setGravity(false);
+        holoBase.setSmall(true);
+        holoBase.setCustomNameVisible(true);
+        holoBase.setCustomName(textComponent.display());
+        hologram.addEntityId(holoBase.getId());
+        entities.add(holoBase);
 
-        for (String line : textComponent.getLines()) {
-          EntityArmorStand holoBase = new EntityArmorStand(worldServer);
-          holoBase.setInvisible(true);
-          holoBase.setArms(false);
-          holoBase.setGravity(false);
-          holoBase.setSmall(true);
-          holoBase.setCustomNameVisible(true);
-          holoBase.setCustomName(line);
-          entities.add(holoBase);
-        }
       }
+
+      if (line instanceof EmptyHologramLine) {
+        entities.add(null);
+      }
+
     }
 
     Collections.reverse(entities);
@@ -130,10 +136,8 @@ public class VersionedHologram extends HologramVersionTemplate {
 
         holoBase.setInvisible(true);
         craftPlayer.getHandle().playerConnection.sendPacket(armorStandMetadataPacket);
-        entityIds.put(hologram, holoBase.getId());
       }
 
-      entityIds.put(hologram, entity.getId());
     }
 
   }
@@ -141,16 +145,32 @@ public class VersionedHologram extends HologramVersionTemplate {
   @Override
   public void despawnHologram(KelpHologram hologram) {
     CraftPlayer player = (CraftPlayer) hologram.getPlayer().getBukkitPlayer();
-    for (Integer entityId : entityIds.get(hologram)) {
+
+    for (HologramLine<?> line : hologram.getSpawnedLines()) {
+      if (line instanceof ItemHologramLine) {
+        ItemHologramLine itemLine = (ItemHologramLine) line;
+        Packet<?> destroyPacket = new PacketPlayOutEntityDestroy(itemLine.getItemEntityId());
+        player.getHandle().playerConnection.sendPacket(destroyPacket);
+      }
+    }
+
+    for (Integer entityId : hologram.getEntityIds()) {
       Packet<?> destroyPacket = new PacketPlayOutEntityDestroy(entityId);
       player.getHandle().playerConnection.sendPacket(destroyPacket);
     }
-    entityIds.removeAll(hologram);
+
+    hologram.getSpawnedLines().clear();
+
   }
 
   @Override
   public void updateHologram(KelpHologram hologram) {
 
+
+  }
+
+  private int randomEntityId() {
+    return ThreadLocalRandom.current().nextInt(20_000, 60_000);
   }
 
 }
